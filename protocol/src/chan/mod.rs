7c8a7a734f849d65c510faf9bf652aa7c3c4e920
fishermen <chenbo@staff.weibo.com>
2021-06-28 10:17:52 +0800

    refactor get and multi-get

--- protocol/src/chan/mod.rs
+++ protocol/src/chan/mod.rs
@@ -1,15 +1,19 @@
 mod get_sync;
 mod multi_get;
+// mod multi_get_2;
 mod operation;
 mod pipeline;
+mod request_ref;
 mod route;
 mod set_sync;
 mod sharding;
 
 pub use get_sync::AsyncGetSync;
-pub use multi_get::AsyncMultiGet;
+pub use multi_get::AsyncMultiGetSharding;
+// pub use multi_get_2::AsyncMultiGet;
 pub use operation::AsyncOperation;
 pub use pipeline::PipeToPingPongChanWrite;
+pub use request_ref::RequestRef;
 pub use route::AsyncRoute;
 pub use set_sync::AsyncSetSync;
 pub use sharding::AsyncSharding;
