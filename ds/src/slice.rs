5caeb386572f793071793bf57f14e28f9434ed39
icycrystal4 <icycrystal4@gmail.com>
2021-07-01 12:46:34 +0800

    把和数据结构相关的统一放到ds下

--- ds/src/slice.rs
+++ ds/src/slice.rs
@@ -1,19 +1,19 @@
 /// 使用者确保Slice持有的数据不会被释放。
 pub struct Slice {
-    ptr: *const u8,
+    ptr: usize,
     len: usize,
 }
 
 impl Slice {
     pub fn from(data: &[u8]) -> Self {
         Self {
-            ptr: data.as_ptr(),
+            ptr: data.as_ptr() as usize,
             len: data.len(),
         }
     }
     #[inline(always)]
     pub fn data(&self) -> &[u8] {
-        unsafe { std::slice::from_raw_parts(self.ptr, self.len) }
+        unsafe { std::slice::from_raw_parts(self.ptr as *const u8, self.len) }
     }
 }
 
@@ -26,9 +26,6 @@ impl AsRef<[u8]> for Slice {
 
 impl Default for Slice {
     fn default() -> Self {
-        Slice {
-            ptr: 0 as *const u8,
-            len: 0,
-        }
+        Slice { ptr: 0, len: 0 }
     }
 }
