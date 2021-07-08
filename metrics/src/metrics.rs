3dfdb91e924080d1fe13fd379a9aeaa840eeddca
mingzhe10 <mingzhe10@staff.weibo.com>
2021-07-08 14:51:20 +0800

    完善计算类型

--- metrics/src/metrics.rs
+++ metrics/src/metrics.rs
@@ -1,3 +1,6 @@
+use std::fmt::{Display, Formatter};
+use std::fmt;
+
 pub(crate) struct MetricsConfig {
     pub(crate) print_only: bool,
     pub(crate) metrics_url: String,
@@ -15,16 +18,27 @@ pub(crate) struct Metrics {
     pub(crate) key: String,
     pub(crate) value: f64,
     pub(crate) count: usize,
+    pub(crate) method: CalculateMethod,
     pub(crate) stat_second: u128,
 }
 
 impl Metrics {
-    pub(crate) fn new(key: String, value: f64, count: usize, stat_second: u128) -> Metrics {
-        Metrics { key, value, count, stat_second }
+    pub(crate) fn new(key: String, value: f64, count: usize, method: CalculateMethod, stat_second: u128) -> Metrics {
+        Metrics { key, value, count, method, stat_second }
     }
 }
 
+#[derive(Clone, Copy)]
 pub(crate) enum CalculateMethod {
     Sum = 0,
     Avg = 1,
+}
+
+impl Display for CalculateMethod {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            CalculateMethod::Sum => write!(f, "Sum"),
+            CalculateMethod::Avg=> write!(f, "Avg"),
+        }
+    }
 }
