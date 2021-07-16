9f64d6babdb617225222ff4a848b33498cb8798e
fishermen <chenbo@staff.weibo.com>
2021-07-16 18:56:48 +0800

    update bkdr and crc32

--- hash/src/bkdr.rs
+++ hash/src/bkdr.rs
@@ -3,11 +3,15 @@ pub struct Bkdr;
 
 impl super::Hash for Bkdr {
     fn hash(&mut self, b: &[u8]) -> u64 {
-        let mut h = 0usize;
-        let seed = 31usize;
+        let mut h = 0i32;
+        let seed = 31i32;
         for c in b.iter() {
-            h = h.wrapping_mul(seed).wrapping_add(*c as usize);
+            h = h.wrapping_mul(seed).wrapping_add(*c as i32);
         }
+        if h < 0 {
+            h = h.wrapping_mul(-1);
+        }
+
         h as u64
     }
 }
