b6f79bc5c7cfd2a50f37a386c77e6b8e4bb4b196
fishermen <chenbo@staff.weibo.com>
2021-08-04 16:57:07 +0800

    update for parsing keys from response

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -47,11 +47,7 @@ pub trait Protocol: Unpin + Clone + 'static {
     // 轮询response，解析出本次查到的keys以及noop所在的位置
     // TODO keys_response ？keys作为返回值 fishermen
     fn scan_response_keys(&self, response: &RingSlice, keys: &mut Vec<String>);
-    fn keys_response<'a, T: Iterator<Item = &'a RingSlice>>(
-        &self,
-        response: T,
-        keys: &mut Vec<String>,
-    );
+    fn keys_response<'a, T: Iterator<Item = &'a RingSlice>>(&self, response: T) -> Vec<String>;
     // 从当前的cmds中，过滤掉已经查到的keys，然后返回新的请求cmds
     // TODO 新的request 作为返回值 fishermen
     fn rebuild_get_multi_request(
