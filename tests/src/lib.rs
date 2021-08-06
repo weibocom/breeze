801c1e4921c373ee6415385a8ebacd440fdd4fcb
icycrystal4 <icycrystal4@gmail.com>
2021-08-06 15:45:43 +0800

    ring slice: 单测修复

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,2 +1,3 @@
 // mod offset;
 mod hash_test;
+mod ring_slice;
--- tests/src/ring_slice.rs
+++ tests/src/ring_slice.rs
@@ -1,7 +1,6 @@
-
 #[cfg(test)]
 mod tests_ds {
-    use super::RingSlice;
+    use ds::RingSlice;
     #[test]
     fn test_ring_slice() {
         let cap = 1024;
@@ -10,28 +9,27 @@ mod tests_ds {
         let ptr = data.as_mut_ptr();
         std::mem::forget(data);
         let mut in_range = RingSlice::from(ptr, cap, 0, 32);
-        let mut buf = vec![0u8; cap];
-        let n = in_range.read(&mut buf);
+        let s = in_range.take_slice();
         assert_eq!(in_range.available(), 0);
-        assert_eq!(&buf[..n], &dc[in_range.start..in_range.end]);
+        assert_eq!(s.data(), &dc[0..32]);
 
         // 截止到末尾的
         let mut end_range = RingSlice::from(ptr, cap, cap - 32, cap);
-        let n = end_range.read(&mut buf);
+        let s = end_range.take_slice();
         assert_eq!(end_range.available(), 0);
-        assert_eq!(&buf[0..n], &dc[end_range.start..end_range.end]);
+        assert_eq!(s.data(), &dc[cap - 32..cap]);
 
         let mut over_range = RingSlice::from(ptr, cap, cap - 32, cap + 32);
-        let n = over_range.read(&mut buf);
+        let s1 = over_range.take_slice();
+        let s2 = over_range.take_slice();
         assert_eq!(over_range.available(), 0);
-        let mut merged = (&dc[cap - 32..]).clone().to_vec();
-        merged.extend(&dc[0..32]);
-        assert_eq!(&buf[0..n], &merged);
+        assert_eq!(s1.data(), &dc[cap - 32..cap]);
+        assert_eq!(s2.data(), &dc[0..32]);
 
         let u32_num = 111234567u32;
         let bytes = u32_num.to_be_bytes();
         unsafe {
-            log::debug!("bytes:{:?}", bytes);
+            //log::debug!("bytes:{:?}", bytes);
             std::ptr::copy_nonoverlapping(bytes.as_ptr(), ptr.offset(8), 4);
             std::ptr::copy_nonoverlapping(bytes.as_ptr(), ptr.offset(1023), 1);
             std::ptr::copy_nonoverlapping(bytes.as_ptr().offset(1), ptr, 3);
