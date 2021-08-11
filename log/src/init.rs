bd9c33b11211045e64e540de1221bcf502c7f86b
icycrystal4 <icycrystal4@gmail.com>
2021-08-11 16:56:30 +0800

    log: 把日志输出到文件，并且进行格式化

--- log/src/init.rs
+++ log/src/init.rs
@@ -1,33 +1,40 @@
-//use elog::LevelFilter;
-//use log4rs::{
-//    append::file::FileAppender,
-//    config::{Appender, Config, Root},
-//    encode::pattern::PatternEncoder,
-//};
-//
-//use std::io::{Error, ErrorKind, Result};
-//use std::path::PathBuf;
-//pub fn init(path: &str) -> Result<()> {
-//    let mut file = PathBuf::new();
-//    file.push(path);
-//    file.push("breeze.log");
-//
-//    let logfile = FileAppender::builder()
-//        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
-//        .build(file)
-//        .unwrap();
-//
-//    let config = Config::builder()
-//        .appender(Appender::builder().build("logfile", Box::new(logfile)))
-//        .build(Root::builder().appender("logfile").build(LevelFilter::Info))
-//        .unwrap();
-//
-//    let _handle = log4rs::init_config(config)
-//        .map_err(|e| Error::new(ErrorKind::InvalidData, format!("init log failed:{:?}", e)))?;
-//
-//    Ok(())
-//}
-use std::io::Result;
-pub fn init(_path: &str) -> Result<()> {
+use elog::LevelFilter;
+use log4rs::{
+    append::file::FileAppender,
+    config::{Appender, Config, Root},
+    encode::pattern::PatternEncoder,
+};
+
+use std::io::{Error, ErrorKind, Result};
+use std::path::PathBuf;
+pub fn init(path: &str) -> Result<()> {
+    let mut file = PathBuf::new();
+    file.push(path);
+    file.push("breeze.log");
+
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "[breeze] {d} - {l} - {t} - {m}{n}",
+        )))
+        .build(file)
+        .unwrap();
+
+    let level = if cfg!(debug_assertions) {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    };
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder().appender("logfile").build(level))
+        .unwrap();
+
+    let _handle = log4rs::init_config(config)
+        .map_err(|e| Error::new(ErrorKind::InvalidData, format!("init log failed:{:?}", e)))?;
+
     Ok(())
 }
+//use std::io::Result;
+//pub fn init(_path: &str) -> Result<()> {
+//    Ok(())
+//}
