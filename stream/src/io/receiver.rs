0d4571d5e5c855ae60e007a55472371731a796ce
icycrystal4 <icycrystal4@gmail.com>
2021-08-24 17:38:08 +0800

    bug fix: 在request buffer进行extends时，需要确保当不存在已接收未处理的请求。避免内存move之后或者释放之后指向错误的或者已经释放的内存

--- stream/src/io/receiver.rs
+++ stream/src/io/receiver.rs
@@ -11,10 +11,8 @@ use std::io::{Error, ErrorKind, Result};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 pub(super) struct Receiver {
-    // 上一个request的读取位置
-    r: usize,
-    // 当前buffer写入的位置
-    w: usize,
+    r: usize, // 上一个request的读取位置
+    w: usize, // 当前buffer写入的位置
     cap: usize,
     buff: Vec<u8>,
     parsed: bool,
@@ -59,9 +57,6 @@ impl Receiver {
     {
         log::debug!("r:{} idx:{} w:{} ", self.r, self.idx, self.w);
         while !self.parsed {
-            if self.w == self.cap {
-                self.extends()?;
-            }
             if self.w > self.r {
                 let (parsed, n) = parser.parse_request(&self.buff[self.r..self.w])?;
                 self.parsed = parsed;
@@ -73,6 +68,9 @@ impl Receiver {
                     break;
                 }
             }
+            // 说明当前已经没有处理中的完整的请求。内存可以安全的move，
+            // 不会导致已有的request，因为move，导致请求失败或者panic.
+            self.try_extends()?;
             // 数据不足。要从stream中读取
             let mut buff = ReadBuf::new(&mut self.buff[self.w..]);
             ready!(reader.as_mut().poll_read(cx, &mut buff))?;
@@ -96,33 +94,25 @@ impl Receiver {
         self.reset();
         Poll::Ready(Ok(()))
     }
+
+    // 调用者需要确保，extends之前，所有完整的请求都已完成处理。不存在处理中的请求，避免内存指向错误, 导致数据异常或者panic。
     // 如果r > 0. 先进行一次move。通过是client端发送的是pipeline请求时会出现这种情况
     // 每次扩容两倍，最多扩容1MB，超过1MB就会扩容失败
-    fn extends(&mut self) -> Result<()> {
-        debug_assert_eq!(self.w, self.cap);
-        log::info!("extend: r:{} idx:{} w:{} ", self.r, self.idx, self.w);
+    #[inline(always)]
+    fn try_extends(&mut self) -> Result<()> {
+        if self.w != self.cap {
+            return Ok(());
+        }
+        if self.r == self.w {
+            log::info!("extends r == w. r:{} idx:{} w:{}", self.r, self.idx, self.w);
+            self.reset();
+            return Ok(());
+        }
         if self.r > 0 {
             self.move_data();
             Ok(())
         } else {
-            if self.cap >= MAX_REQUEST_SIZE {
-                log::warn!("request size limited:{} >= {}", self.cap, MAX_REQUEST_SIZE);
-                Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    "max request size limited: 1mb",
-                ))
-            } else {
-                // 说明容量不足。需要扩展
-                let cap = self.buff.len();
-                let cap = (cap * 2).min(MAX_REQUEST_SIZE);
-                let mut new_buff = vec![0u8; cap];
-                use std::ptr::copy_nonoverlapping as copy;
-                unsafe { copy(self.buff.as_ptr(), new_buff.as_mut_ptr(), self.buff.len()) };
-                self.buff.clear();
-                self.buff = new_buff;
-                self.cap = cap;
-                Ok(())
-            }
+            self.extend()
         }
     }
     #[inline]
@@ -144,5 +134,35 @@ impl Receiver {
         self.idx = 0;
         self.r = 0;
         self.w = len;
+    }
+    #[inline]
+    fn extend(&mut self) -> Result<()> {
+        log::info!(
+            "extend: r:{} idx:{} w:{} cap: from {} to {}",
+            self.r,
+            self.idx,
+            self.w,
+            self.cap,
+            2 * self.cap,
+        );
+        if self.cap >= MAX_REQUEST_SIZE {
+            log::warn!("request size limited:{} >= {}", self.cap, MAX_REQUEST_SIZE);
+            Err(Error::new(
+                ErrorKind::InvalidInput,
+                "max request size limited: 1mb",
+            ))
+        } else {
+            // 说明容量不足。需要扩展
+            let cap = self.buff.len();
+            let cap = (cap * 2).min(MAX_REQUEST_SIZE);
+            let mut new_buff = vec![0u8; cap];
+            use std::ptr::copy_nonoverlapping as copy;
+            unsafe { copy(self.buff.as_ptr(), new_buff.as_mut_ptr(), self.buff.len()) };
+            // 如果还存在处理中的请求，buff clear掉之后可能指向一段已释放的内存。
+            self.buff.clear();
+            self.buff = new_buff;
+            self.cap = cap;
+            Ok(())
+        }
     }
 }
