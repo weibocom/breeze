f9b763f4b1c37b78e9aa103a13cc5685da1abfd1
icycrystal4 <icycrystal4@gmail.com>
2021-09-05 21:30:16 +0800

    为Vec<u8>添加一个高效的写入字符串的接口定义

--- ds/src/lib.rs
+++ ds/src/lib.rs
@@ -9,3 +9,22 @@ pub use cid::*;
 pub use offset::*;
 pub use ring::{ResizedRingBuffer, RingBuffer, RingSlice};
 pub use slice::*;
+
+pub trait Buffer {
+    fn write<D: AsRef<[u8]>>(&mut self, data: D);
+}
+
+impl Buffer for Vec<u8> {
+    fn write<D: AsRef<[u8]>>(&mut self, data: D) {
+        let b = data.as_ref();
+        use std::ptr::copy_nonoverlapping as copy;
+        unsafe {
+            copy(
+                b.as_ptr() as *const u8,
+                self.as_mut_ptr().offset(self.len() as isize),
+                b.len(),
+            );
+            self.set_len(self.len() + b.len());
+        }
+    }
+}
