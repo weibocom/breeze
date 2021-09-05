d030a53b715e847adbb3f5faa3d42280d1c7269e
icycrystal4 <icycrystal4@gmail.com>
2021-09-05 22:45:15 +0800

    bug fix: vec没有reserve

--- ds/src/lib.rs
+++ ds/src/lib.rs
@@ -18,6 +18,7 @@ impl Buffer for Vec<u8> {
     fn write<D: AsRef<[u8]>>(&mut self, data: D) {
         let b = data.as_ref();
         use std::ptr::copy_nonoverlapping as copy;
+        self.reserve(b.len());
         unsafe {
             copy(
                 b.as_ptr() as *const u8,
