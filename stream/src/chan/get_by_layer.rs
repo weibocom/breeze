3e764ae00f6086d9a9da6b9c3c9f60612cd44b9f
xinxin22 <xinxin22@staff.weibo.com>
2021-09-17 18:42:34 +0800

    back_qps

--- stream/src/chan/get_by_layer.rs
+++ stream/src/chan/get_by_layer.rs
@@ -90,11 +90,11 @@ where
         // 构建回种的请求
         if self.idx > 0 {
             self.create_requests_wb(&item);
-            self.do_write_back(cx);
             //记录回种的kps，metric
             if let Some(reqs_wb) = self.requests_writeback.as_mut() {
                 metrics::qps("back_kps", reqs_wb.len(), item.rid().metric_id());
             }
+            self.do_write_back(cx);
         }
 
         let found = item.keys_num();
