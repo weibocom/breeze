4eac8e13e1d92272927cc62b0ed091ff624c63fd
icycrystal4 <icycrystal4@gmail.com>
2021-09-19 20:07:15 +0800

    把done与timeout的两个检查线程合二为一，减少一个线程的消耗。在资源释放时，流的关闭分成两个阶段：1. 立即close，此时新进来的请求会快速失败。原有的请求会继续处理；2. 15秒钟后，关闭连接，释放相关的资源

--- stream/src/backend/check.rs
+++ stream/src/backend/check.rs
@@ -3,16 +3,22 @@ use ds::{Cid, Ids};
 use protocol::{Protocol, Resource};
 
 use std::io::Result;
-use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
-use std::sync::Arc;
-use std::time::Duration;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use std::time::{Duration, Instant};
 
 use tokio::net::TcpStream;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
-use tokio::time::sleep;
+use tokio::time::{interval_at, Interval};
+
+use futures::executor::block_on;
 
 pub struct BackendBuilder {
-    checker: Arc<BackendChecker>,
+    finished: Arc<AtomicBool>,
+    inited: Arc<AtomicBool>,
+    stream: Arc<RingBufferStream>,
     ids: Arc<Ids>,
 }
 
@@ -21,105 +27,115 @@ impl BackendBuilder {
     where
         P: Unpin + Send + Sync + Protocol + 'static + Clone,
     {
+        let finished = Arc::new(AtomicBool::new(false));
+        let init = Arc::new(AtomicBool::new(false));
         let stream = Arc::new(RingBufferStream::with_capacity(parallel, biz, addr, rsrc));
-        let (tx, rx) = channel(8);
-        let checker = Arc::new(BackendChecker::from(stream.clone(), tx));
-        checker.clone().start_check(parser.clone(), rx);
-        checker.clone().start_check_timeout();
-        let me = Self {
-            checker: checker,
+        let checker = BackendChecker::from(stream.clone(), finished.clone(), init.clone(), parser);
+        tokio::spawn(async { checker.await });
+
+        Self {
+            stream: stream,
             ids: Arc::new(Ids::with_capacity(parallel)),
-        };
-        me
+            finished: finished,
+            inited: init,
+        }
     }
     pub fn build(&self) -> BackendStream {
         self.ids
             .next()
-            .map(|cid| {
-                BackendStream::from(Cid::new(cid, self.ids.clone()), self.checker.inner.clone())
-            })
+            .map(|cid| BackendStream::from(Cid::new(cid, self.ids.clone()), self.stream.clone()))
             .unwrap_or_else(|| {
-                log::info!("connection id overflow. {}", self.checker.addr());
+                log::info!("connection id overflow. {}", self.stream.addr());
                 BackendStream::not_connected()
             })
     }
     // 已经连接上或者至少连接了一次
     #[inline]
     pub fn inited(&self) -> bool {
-        !self.checker.done() || self.checker.num.load(Ordering::Acquire) >= 1
+        self.inited.load(Ordering::Acquire)
     }
 }
 
 impl Drop for BackendBuilder {
     fn drop(&mut self) {
-        self.checker.finished.store(true, Ordering::Release);
-        // 结束流处理。
-        self.checker.try_close();
+        self.finished.store(true, Ordering::Release);
     }
 }
 
-pub struct BackendChecker {
+pub struct BackendChecker<P> {
     inner: Arc<RingBufferStream>,
+    rx: Receiver<u8>,
     tx: Arc<Sender<u8>>,
-    finished: AtomicBool,
-    num: AtomicUsize, // 建立连接的数量。
+    connecting: bool, // 当前是否需要重新建立连接
+    finished: Arc<AtomicBool>,
+    inited: Arc<AtomicBool>,
+    instant_conn: Instant,    // 最近一次连接开始时间
+    instant_timeout: Instant, // 最近一次开始计算timeout的时间
+    tries: usize,             // 最近一次连接失败的次数
+    tick: Interval,
+    req_num: usize, // 检查timeout时，上一次的请求号。
+    parser: P,
+    close: Instant, // 关闭流的时间
+    complete: bool,
 }
 
-impl BackendChecker {
-    fn from(stream: Arc<RingBufferStream>, tx: Sender<u8>) -> Self {
-        if let Err(e) = tx.try_send(0) {
-            log::error!("failed to send connect signal to {}:{:?}", stream.addr(), e);
-        }
-        let me = Self {
+impl<P> BackendChecker<P> {
+    fn from(
+        stream: Arc<RingBufferStream>,
+        finished: Arc<AtomicBool>,
+        inited: Arc<AtomicBool>,
+        parser: P,
+    ) -> Self {
+        let (tx, rx) = channel(8);
+        // 用一个random的耗时，将不同的backend的tick错开。
+        const SECS: u64 = 3;
+        let latency_ms: u64 = rand::random::<u64>() % (SECS * 1000);
+        let start = Instant::now() + Duration::from_millis(latency_ms);
+        let tick = interval_at(start.into(), Duration::from_secs(SECS));
+        let (req_num, _) = stream.load_ping_ping();
+        Self {
             tx: Arc::new(tx),
+            rx: rx,
             inner: stream,
-            finished: AtomicBool::new(false),
-            num: AtomicUsize::new(0),
-        };
-        me
+            req_num: req_num,
+            tries: 0,
+            connecting: true, // 初始化时需要建立连接
+            instant_conn: Instant::now(),
+            instant_timeout: Instant::now(),
+            tick: tick,
+            finished: finished,
+            inited: inited,
+            parser: parser,
+            complete: false,
+            close: Instant::now(),
+        }
     }
-
-    // 在mpmc::RingBufferStream的done变为true时，会向rx发送一个信号。
-    fn start_check<P>(self: Arc<Self>, parser: P, mut rx: Receiver<u8>)
+    fn connect(&mut self) -> bool
     where
         P: Unpin + Send + Sync + Protocol + 'static + Clone,
     {
-        tokio::spawn(async move {
-            while let Some(_) = rx.recv().await {
-                if self.finished.load(Ordering::Acquire) {
-                    break;
-                }
-                log::debug!("signal recived, try to connect:{}", self.inner.addr());
-                self.connect(parser.clone()).await;
-            }
-            log::info!("check done complete:{}", self.inner.addr());
-        });
-    }
-
-    async fn connect<P>(&self, parser: P)
-    where
-        P: Unpin + Send + Sync + Protocol + 'static + Clone,
-    {
-        let mut tries = 0;
-        while !self.finished.load(Ordering::Acquire) {
-            match self.check_reconnected_once(parser.clone()).await {
+        if self.tries == 0 {
+            self.instant_conn = Instant::now();
+        }
+        let expected = Duration::from_secs(self.tries.min(31) as u64);
+        self.tries += 1;
+        let mut succ = false;
+        if self.instant_conn.elapsed() >= expected {
+            log::debug!("try to connect {} tries:{}", self.addr(), self.tries);
+            match block_on(async { self.reconnected_once().await }) {
                 Ok(_) => {
-                    log::debug!("check: {} connected tries:{}", self.addr(), tries);
-                    break;
+                    succ = true;
+                    self.tries = 0;
                 }
                 Err(e) => {
-                    log::info!("connect {} failed:{:?} tries:{}", self.addr(), e, tries);
-                    let secs = 1 << tries.min(5);
-                    tries += 1;
-                    self.num.fetch_add(1, Ordering::AcqRel);
-                    metrics::status("status", metrics::Status::Down, self.metric_id());
-                    sleep(Duration::from_secs(secs)).await;
+                    log::warn!("failed to connect {} err:{}", self.addr(), e);
                 }
-            }
+            };
         }
+        self.inited.store(true, Ordering::Release);
+        succ
     }
-
-    async fn check_reconnected_once<P>(&self, parser: P) -> Result<()>
+    async fn reconnected_once(&self) -> Result<()>
     where
         P: Unpin + Send + Sync + Protocol + 'static + Clone,
     {
@@ -132,7 +148,7 @@ impl BackendChecker {
         let req_stream = self.inner.clone();
 
         req_stream.bridge(
-            parser.clone(),
+            self.parser.clone(),
             r,
             w,
             Notifier {
@@ -141,50 +157,36 @@ impl BackendChecker {
         );
         Ok(())
     }
-    fn start_check_timeout(self: Arc<Self>) {
-        log::debug!("check: {} timeout task started", self.addr());
-        tokio::spawn(async move {
-            self._start_check_timeout().await;
-        });
-    }
 
     // 满足以下所有条件，则把done调整为true，当前实例快速失败。
     // 1. req_num停止更新；
     // 2. resp_num > req_num;
     // 3. 超过7秒钟。why 7 secs?
-    async fn _start_check_timeout(self: Arc<Self>) {
-        use std::time::Instant;
+    fn check_timeout(&mut self) {
         const TIME_OUT: Duration = Duration::from_secs(4);
-
-        let (mut last_req, _) = self.inner.load_ping_ping();
-        let mut duration = Instant::now();
-        while !self.finished.load(Ordering::Acquire) {
-            sleep(Duration::from_secs(1)).await;
-            // 已经done了，忽略
-            let done = self.inner.done();
-            let (req_num, resp_num) = self.inner.load_ping_ping();
-            if done || req_num != last_req || resp_num == req_num {
-                last_req = req_num;
-                duration = Instant::now();
-                continue;
-            }
-            // 判断是否超时
-            let elap = duration.elapsed();
-            if elap <= TIME_OUT {
-                // 还未超时
-                continue;
-            }
-            log::error!(
-                "check-timeout: no response return in {:?}. stream marked done. req:{} resp:{}, addr:{}",
-                elap,
-                req_num,
-                resp_num,
-                self.addr()
-            );
-            self.inner.mark_done();
-            metrics::status("status", metrics::Status::Timeout, self.metric_id());
+        // 已经done了，忽略
+        let done = self.inner.done();
+        let (req_num, resp_num) = self.inner.load_ping_ping();
+        if done || req_num != self.req_num || resp_num == req_num {
+            self.req_num = req_num;
+            self.instant_timeout = Instant::now();
+            return;
         }
-        log::info!("timeout task checker complete:{}", self.addr());
+        // 判断是否超时
+        let elap = self.instant_timeout.elapsed();
+        if elap <= TIME_OUT {
+            // 还未超时
+            return;
+        }
+        log::error!(
+            "timeout: no response return in {:?}. stream marked done. req:{} resp:{}, addr:{}",
+            elap,
+            req_num,
+            resp_num,
+            self.addr()
+        );
+        self.inner.mark_done();
+        metrics::status("status", metrics::Status::Timeout, self.metric_id());
     }
 }
 
@@ -201,9 +203,58 @@ pub struct Notifier {
     tx: Arc<Sender<u8>>,
 }
 
-impl std::ops::Deref for BackendChecker {
+impl<P> std::ops::Deref for BackendChecker<P> {
     type Target = RingBufferStream;
     fn deref(&self) -> &Self::Target {
         &self.inner
+    }
+}
+
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use futures::ready;
+
+impl<P> Future for BackendChecker<P>
+where
+    P: Unpin + Send + Sync + Protocol + 'static + Clone,
+{
+    type Output = ();
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let me = &mut self;
+        // 没有结束，就一直check
+        while !me.finished.load(Ordering::Acquire) {
+            if me.connecting {
+                if me.connect() {
+                    me.connecting = false;
+                }
+            }
+            me.check_timeout();
+            match me.rx.poll_recv(cx) {
+                Poll::Ready(Some(_)) => {
+                    me.connecting = true;
+                    me.instant_timeout = Instant::now();
+                }
+                _ => {}
+            }
+            ready!(me.tick.poll_tick(cx));
+        }
+
+        if !me.complete {
+            log::info!(
+                "task finished {}. stream closed immediately, and shutdown in 15seconds.",
+                me.addr()
+            );
+            me.complete = true;
+            me.try_close();
+            me.close = Instant::now();
+        }
+        while me.close.elapsed() <= Duration::from_secs(15) {
+            ready!(me.tick.poll_tick(cx));
+        }
+        log::info!("stream shutting down. {}", me.addr());
+        me.shutdown_all();
+        Poll::Ready(())
     }
 }
