0bb42e36fea646666b09f836c44e611096699f1b
hustfisher <hustfisher@gmail.com>
2021-11-25 16:54:14 +0800

    refactor endpoint

--- endpoint/src/topology/mod.rs
+++ endpoint/src/topology/mod.rs
@@ -1,10 +1,77 @@
+mod inner;
+mod layer;
+mod seq;
+
+use std::{
+    collections::HashMap,
+    io::{Error, ErrorKind, Result},
+};
+
+use discovery::Inited;
+use protocol::{Protocol, Resource};
 use stream::LayerRole;
 
-mod inner;
-
-mod layer;
 pub use inner::Inner;
 pub use layer::Layer;
+pub use seq::Seq;
+
+use crate::{cacheservice::MemcacheTopology, redisservice::RedisTopology};
+
+#[derive(Clone)]
+pub enum Topology<P> {
+    RedisTopo(RedisTopology<P>),
+    MemcacheTopo(MemcacheTopology<P>),
+}
+
+impl<P> Topology<P>
+where
+    P: Protocol,
+{
+    pub fn try_from(parser: P, endpoint: String) -> Result<Self> {
+        match &endpoint[..] {
+            "rs" => Ok(Self::RedisTopo(parser.into())),
+            "cs" => Ok(Self::MemcacheTopo(parser.into())),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                format!("'{}' is not a valid endpoint", endpoint),
+            )),
+        }
+    }
+}
+
+impl<P> Inited for Topology<P> {
+    fn inited(&self) -> bool {
+        match self {
+            Self::RedisTopo(r) => r.inited(),
+            Self::MemcacheTopo(c) => c.inited(),
+        }
+    }
+}
+
+impl<P> discovery::TopologyWrite for Topology<P>
+where
+    P: Sync + Send + Protocol,
+{
+    fn resource(&self) -> protocol::Resource {
+        match self {
+            Self::RedisTopo(_) => Resource::Redis,
+            Self::MemcacheTopo(_) => Resource::Memcache,
+        }
+    }
+    fn update(&mut self, name: &str, cfg: &str, hosts: &HashMap<String, Vec<String>>) {
+        match self {
+            Self::RedisTopo(r) => discovery::TopologyWrite::update(r, name, cfg, hosts),
+            Self::MemcacheTopo(c) => discovery::TopologyWrite::update(c, name, cfg, hosts),
+        }
+    }
+
+    fn gc(&mut self) {
+        match self {
+            Self::RedisTopo(r) => discovery::TopologyWrite::gc(r),
+            Self::MemcacheTopo(c) => discovery::TopologyWrite::gc(c),
+        }
+    }
+}
 
 pub(crate) trait VisitAddress {
     fn visit<F: FnMut(&str)>(&self, f: F);
