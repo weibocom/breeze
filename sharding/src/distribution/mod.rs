b9d8747e6fb74c44488cb7bd2be24b8493c0ecce
hustfisher <hustfisher@gmail.com>
2021-12-24 15:59:10 +0800

    support redis protocol

--- sharding/src/distribution/mod.rs
+++ sharding/src/distribution/mod.rs
@@ -1,20 +1,25 @@
 mod consistent;
-use consistent::Consistent;
-
 mod modula;
-use modula::Modula;
+mod range;
 
-#[derive(Clone)]
+use consistent::Consistent;
+use modula::Modula;
+use range::Range;
+
+#[derive(Clone, Debug)]
 pub enum Distribute {
     Consistent(Consistent),
     Modula(Modula),
+    Range(Range),
 }
 
 impl Distribute {
     pub fn from(distribution: &str, names: &Vec<String>) -> Self {
-        match distribution {
-            "modula" | "Modula" | "MODULA" => Self::Modula(Modula::from(names.len())),
-            "ketama" | "Ketama" | "KETAMA" => Self::Consistent(Consistent::from(&names)),
+        let dist = distribution.to_ascii_lowercase();
+        match dist.as_str() {
+            "modula" => Self::Modula(Modula::from(names.len())),
+            "ketama" => Self::Consistent(Consistent::from(names)),
+            "range" => Self::Range(Range::from(names.len())),
             _ => {
                 log::warn!("'{}' is not valid , use modula instead", distribution);
                 Self::Modula(Modula::from(names.len()))
@@ -26,6 +31,7 @@ impl Distribute {
         match self {
             Self::Consistent(d) => d.index(hash),
             Self::Modula(d) => d.index(hash),
+            Self::Range(r) => r.index(hash),
         }
     }
 }
