64576dbedcb9082fdcc18102d3487347b469fcab
hustfisher <hustfisher@gmail.com>
2021-12-28 16:06:29 +0800

    重构range hash

--- sharding/src/distribution/range.rs
+++ sharding/src/distribution/range.rs
@@ -14,8 +14,15 @@ impl Range {
         }
     }
 
-    pub fn index(&self, hash: u64) -> usize {
-        let rs = hash / self.interval;
+    pub fn index(&self, hash: i64) -> usize {
+        let mut val = hash
+            .wrapping_div(super::DIST_RANGE_SPLIT_DEFAULT)
+            .wrapping_rem(super::DIST_RANGE_SPLIT_DEFAULT);
+        if val < 0 {
+            log::warn!("found negative crc range pre hash:{:?}", val);
+            val = val.wrapping_abs();
+        }
+        let rs = val as u64 / self.interval;
         rs as usize
     }
 }
