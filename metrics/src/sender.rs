d39c3bccd91b6d3460acfc78a92f4f1736aa9118
icycrystal4 <icycrystal4@gmail.com>
2021-12-28 11:18:27 +0000

    调整部分日志格式

--- metrics/src/sender.rs
+++ metrics/src/sender.rs
@@ -40,7 +40,8 @@ impl Future for Sender {
         loop {
             ready!(me.tick.poll_tick(cx));
             // 判断是否可以flush
-            let elapsed = me.last.elapsed().as_secs_f64();
+            let elapsed = me.last.elapsed().as_secs_f64().round();
+            assert!(elapsed > 0f64);
             let metrics = crate::get_metrics();
             metrics.write(&mut me.packet, elapsed);
             crate::Host::snapshot(&mut me.packet, elapsed);
