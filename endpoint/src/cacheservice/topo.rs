980fe3d3100251d707da9662b4644fc6aaafbe95
hustfisher <hustfisher@gmail.com>
2021-12-28 18:06:24 +0800

    1 update log; 2 support pipeline

--- endpoint/src/cacheservice/topo.rs
+++ endpoint/src/cacheservice/topo.rs
@@ -163,11 +163,6 @@ where
                 return;
             }
             self.hasher = Hasher::from(&ns.hash);
-            log::debug!(
-                "++++++ update memcache config with hash:{}: config:{}",
-                ns.hash,
-                cfg
-            );
             let dist = &ns.distribution;
 
             let old_streams = self.streams.split_off(0);
