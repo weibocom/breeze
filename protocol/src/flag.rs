3a4cb334c6667a2394278efcf41875f2daf08ba6
icycrystal4 <icycrystal4@gmail.com>
2021-12-31 12:26:43 +0000

    flag: 把flag分为两个部分，一个是能用的，一个是ext给不同协议使用。 提供一个简化版的只支持单key的redis协议有限支持。

--- protocol/src/flag.rs
+++ protocol/src/flag.rs
@@ -1,86 +1,22 @@
-use crate::Operation;
-#[derive(Debug)]
+use crate::{OpCode, Operation};
+#[derive(Debug, Default)]
 pub struct Flag {
+    op_code: OpCode,
+    op: Operation,
     v: u64,
 }
-
-// 高5字节，归各种协议私用，低3个字节，共享
-const RSP_BIT_META_LEN: u8 = 40;
-const RSP_BIT_TOKEN_LEN: u8 = 48;
-const REQ_BIT_PADDING_RSP: u8 = 32;
-const REQ_BIT_MKEY_FIRST: u8 = 24;
-// const REQ_BIT_VAL_MKEY_FIRST: u64 = 1 << 24;
 
 impl Flag {
     // first = true 满足所有条件1. 当前请求是multiget；2. 拆分了多个子请求；3. 是`第一`个子请求；
     // last  = true 满足所有条件1. 当前请求是multiget；2. 拆分了多个子请求；3. 是`最后`一个子请求；
     #[inline(always)]
-    pub fn from_op(op_code: u8, op: Operation) -> Self {
-        let v = ((op_code as u64) << 8) | (op as u64);
-        Self { v }
+    pub fn from_op(op_code: OpCode, op: Operation) -> Self {
+        Self { op_code, op, v: 0 }
     }
-
-    // **********************  TODO: redis相关，需要按协议分拆 start **********************
-    // TODO: 后续flag需要根据协议进行分别构建 speedup fishermen
-    #[inline(always)]
-    pub fn from_mkey_op(mkey_first: bool, padding_rsp: u8, op: Operation) -> Self {
-        let mut v = 0u64;
-        // 只有多个key时，first key才有意义
-        if mkey_first {
-            const MKEY_FIRST_VAL: u64 = 1 << REQ_BIT_MKEY_FIRST;
-            v |= MKEY_FIRST_VAL;
-        }
-
-        if padding_rsp > 0 {
-            v |= (padding_rsp as u64) << REQ_BIT_PADDING_RSP;
-        }
-
-        // operation 需要小雨
-        v |= op as u64;
-
-        Self { v }
-    }
-
-    // meta len 正常不会超过20，256绰绰有余
-    pub fn from_metalen_tokencount(metalen: u8, tokencount: u8) -> Self {
-        let mut v = 0u64;
-        if metalen > 0 {
-            v |= (metalen as u64) << RSP_BIT_META_LEN;
-        }
-        if tokencount > 0 {
-            v |= (tokencount as u64) << RSP_BIT_TOKEN_LEN;
-        }
-        Self { v }
-    }
-
-    pub fn padding_rsp(&self) -> u8 {
-        const MASK: u64 = (1 << 8) - 1;
-        let p = (self.v >> REQ_BIT_PADDING_RSP) & MASK;
-        p as u8
-    }
-
-    pub fn is_mkey_first(&self) -> bool {
-        const MASK_MKF: u64 = 1 << REQ_BIT_MKEY_FIRST;
-        (self.v & MASK_MKF) == MASK_MKF
-    }
-
-    pub fn meta_len(&self) -> u8 {
-        const MASK: u64 = (1 << 8) - 1;
-        let len = (self.v >> RSP_BIT_META_LEN) & MASK;
-        len as u8
-    }
-
-    pub fn token_count(&self) -> u8 {
-        const MASK: u64 = (1u64 << 8) - 1;
-        let len = (self.v >> RSP_BIT_TOKEN_LEN) & MASK;
-        len as u8
-    }
-
-    // **********************  TODO: redis相关，需要按协议分拆 end！speedup **********************
 
     #[inline(always)]
     pub fn new() -> Self {
-        Self { v: 0 }
+        Self::default()
     }
     // 低位第一个字节是operation位
     // 第二个字节是op_code
@@ -108,12 +44,11 @@ impl Flag {
     }
     #[inline(always)]
     pub fn operation(&self) -> Operation {
-        (self.v as u8).into()
+        self.op
     }
     #[inline(always)]
-    pub fn op_code(&self) -> u8 {
-        // 第二个字节是op_code
-        (self.v >> 8) as u8
+    pub fn op_code(&self) -> OpCode {
+        self.op_code
     }
     #[inline(always)]
     pub fn set_noforward(&mut self) -> &mut Self {
@@ -135,7 +70,20 @@ impl Flag {
         self.v & m == m
     }
     #[inline(always)]
-    pub fn reset_flag(&mut self, op_code: u8, op: Operation) {
-        *self = Self::from_op(op_code, op);
+    pub fn reset_flag(&mut self, op_code: OpCode, op: Operation) {
+        self.op_code = op_code;
+        self.op = op;
+    }
+    #[inline(always)]
+    pub fn set_ext(&mut self, ext: u64) {
+        self.v = ext;
+    }
+    #[inline(always)]
+    pub fn ext(&self) -> u64 {
+        self.v
+    }
+    #[inline(always)]
+    pub fn ext_mut(&mut self) -> &mut u64 {
+        &mut self.v
     }
 }
--- protocol/src/memcache/binary/mod.rs
+++ protocol/src/memcache/binary/mod.rs
@@ -38,7 +38,7 @@ impl Protocol for MemcacheBinary {
                 data.update(idx, mapped_op_code);
             }
             // 存储原始的op_code
-            let mut flag = Flag::from_op(op_code, COMMAND_IDX[op_code as usize].into());
+            let mut flag = Flag::from_op(op_code as u16, COMMAND_IDX[op_code as usize].into());
             if NOREPLY_MAPPING[req.op() as usize] == req.op() {
                 flag.set_sentonly();
             }
@@ -72,7 +72,7 @@ impl Protocol for MemcacheBinary {
             let r = data.slice();
             let pl = r.packet_len();
             if len >= pl {
-                let mut flag = Flag::from_op(r.op(), r.operation());
+                let mut flag = Flag::from_op(r.op() as u16, r.operation());
                 if r.status_ok() {
                     flag.set_status_ok();
                 }
@@ -97,7 +97,7 @@ impl Protocol for MemcacheBinary {
         }
         if old_op_code != resp.op_code() {
             // 更新resp opcode
-            resp.data_mut().update_opcode(old_op_code);
+            resp.data_mut().update_opcode(old_op_code as u8);
         }
         let len = resp.len();
         let mut oft = 0;
@@ -131,7 +131,7 @@ impl Protocol for MemcacheBinary {
         req: &HashedCommand,
         w: &mut W,
     ) -> Result<()> {
-        match req.op_code() {
+        match req.op_code() as u8 {
             OP_CODE_NOOP => {
                 // 第一个字节变更为Response，其他的与Request保持一致
                 w.write_u8(RESPONSE_MAGIC)?;
@@ -156,7 +156,7 @@ impl MemcacheBinary {
         data.clear_cas();
         // 更新flag
         let op = COMMAND_IDX[op_code as usize].into();
-        req.reset_flag(op_code, op);
+        req.reset_flag(op_code as u16, op);
         // 设置只发送标签，发送完成即请求完成。
         req.set_sentonly();
         debug_assert!(req.operation().is_store());
@@ -203,7 +203,7 @@ impl MemcacheBinary {
         rsp_cmd.value().copy_to_vec(&mut req_cmd);
 
         let hash = req.hash();
-        let mut flag = Flag::from_op(op, COMMAND_IDX[op as usize].into());
+        let mut flag = Flag::from_op(op as u16, COMMAND_IDX[op as usize].into());
         flag.set_sentonly();
 
         let guard = ds::MemGuard::from_vec(req_cmd);
