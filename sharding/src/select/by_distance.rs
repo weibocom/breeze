c506e8971fa9c38a3f7aa946f8558a4b523b4610
icycrystal4 <icycrystal4@gmail.com>
2022-01-07 07:56:31 +0000

    sharding-select: 优先访问B断相关的，然后按距离选择。 local ip的选择策略：至少1/3的副本作为local访问

--- sharding/src/select/by_distance.rs
+++ sharding/src/select/by_distance.rs
@@ -8,8 +8,7 @@ use std::sync::Arc;
 pub struct Distance<T> {
     // batch: 至少在一个T上连续连续多少次
     batch_shift: u8,
-    idx_local: u16,
-    idx_remote: u16,
+    len_local: u16,
     seq: Arc<AtomicUsize>,
     pub(super) replicas: Vec<T>,
 }
@@ -19,38 +18,31 @@ impl<T: Addr> Distance<T> {
         let batch = 1024usize;
         debug_assert_ne!(replicas.len(), 0);
         // 把replica排序、分成3组: local、remote、跨region。
-        let distances: HashMap<String, u8> = replicas
+        let distances: HashMap<String, u16> = replicas
             .iter()
             .map(|t| (t.addr().to_string(), distance(t.addr())))
             .collect();
         replicas.sort_by(|a, b| distances[a.addr()].cmp(&distances[b.addr()]));
-        let mut idx_local = 0;
-        let mut idx_remote = 0;
-        for r in replicas.iter() {
-            let d = distances[r.addr()];
-            if d == LOCAL {
-                idx_local += 1;
+        // 至少取1/3的资源代码库local，1/3以后与local距离相同的也计算到local内
+        let mut len_local = (replicas.len() / 3).max(1);
+        let farest = distances[replicas[len_local - 1].addr()];
+        for r in &replicas[len_local..] {
+            if farest != distances[r.addr()] {
+                break;
             }
-            if d == REMOTE {
-                idx_remote += 1;
-            }
-        }
-        idx_remote += idx_local;
-        if idx_local == 0 {
-            idx_local = idx_remote;
+            len_local += 1;
         }
         let seq = Arc::new(AtomicUsize::new(rand::random::<u16>() as usize));
         // 最小是1，最大是65536
         let batch_shift = batch.max(1).next_power_of_two().min(65536).trailing_zeros() as u8;
         log::info!(
-            "local:{} remote:{} first:{:?}",
-            idx_local,
-            idx_remote,
+            "local:{} {} {:?}",
+            len_local,
+            replicas.len(),
             replicas[0].addr()
         );
         Self {
-            idx_local,
-            idx_remote,
+            len_local: len_local as u16,
             replicas,
             batch_shift,
             seq,
@@ -58,38 +50,52 @@ impl<T: Addr> Distance<T> {
     }
     #[inline(always)]
     pub(super) fn len(&self) -> usize {
-        self.idx_remote as usize
+        self.replicas.len()
+    }
+    #[inline(always)]
+    fn local_len(&self) -> usize {
+        self.len_local as usize
+    }
+    #[inline(always)]
+    fn remote_len(&self) -> usize {
+        self.len() - self.local_len()
     }
     // 只从local获取
     #[inline(always)]
     pub unsafe fn unsafe_select(&self) -> (usize, &T) {
         debug_assert_ne!(self.len(), 0);
-        let idx = (self.seq.fetch_add(1, Ordering::Relaxed) >> self.batch_shift as usize)
-            % self.idx_local as usize;
+        let idx = if self.len() == 0 {
+            0
+        } else {
+            (self.seq.fetch_add(1, Ordering::Relaxed) >> self.batch_shift as usize)
+                % self.local_len()
+        };
         (idx, self.replicas.get_unchecked(idx))
     }
     #[inline(always)]
     pub unsafe fn unsafe_next(&self, idx: usize, runs: usize) -> (usize, &T) {
         debug_assert!(runs < self.len());
         // 还可以从local中取
-        let idx = if runs < self.idx_local as usize {
-            (idx + 1) % self.idx_local as usize
+        let idx = if runs < self.local_len() {
+            (idx + 1) % self.local_len()
         } else {
-            // 从remote中取
-            (runs as usize) % self.len()
+            debug_assert_ne!(self.local_len(), self.len());
+            if idx == self.local_len() {
+                // 从[idx_local..idx_remote)随机取一个
+                self.seq.fetch_add(1, Ordering::Relaxed) % self.remote_len() + self.local_len()
+            } else {
+                if idx + 1 == self.len() {
+                    self.local_len()
+                } else {
+                    idx + 1
+                }
+            }
         };
         (idx, self.replicas.get_unchecked(idx))
     }
 }
 
-const LOCAL: u8 = 1;
-const REMOTE: u8 = 2;
-const FAR: u8 = 3;
-
-// 0：local  （优先访问本地）
-// 1: remote  (remote:本地异常后访问）
-// 2: 异地。（无论如何都不访问）
-fn distance(t: &str) -> u8 {
+fn distance(t: &str) -> u16 {
     DISTANCE_CALCULATOR
         .get()
         .expect("not init")
@@ -113,32 +119,30 @@ static DISTANCE_CALCULATOR: OnceCell<CowReadHandle<DistanceCalculator>> = OnceCe
 static DISTANCE_CALCULATOR_W: OnceCell<Mutex<CowWriteHandle<DistanceCalculator>>> = OnceCell::new();
 
 type Address = String;
-type City = String;
 type Zone = String;
 #[derive(Clone, Default, Debug)]
 pub struct DistanceCalculator {
-    local_city: String,
+    local_b_class: String, // 本地的b类地址
     local_zone: String,
-    zones: HashMap<Address, (City, Zone)>,
+    // 不同zone之间的距离。
+    distances: HashMap<Zone, HashMap<Zone, u16>>,
+    zones: HashMap<Address, Zone>,
 }
 
 impl DistanceCalculator {
-    fn distance(&self, addr: &str) -> u8 {
-        let addr = self.b_class(addr);
-        match self.zones.get(addr) {
-            None => REMOTE,
-            Some((city, zone)) => {
-                if self.local_city.len() > 0 && city.len() > 0 && &self.local_city != city {
-                    FAR
-                } else {
-                    if zone == &self.local_zone {
-                        LOCAL
-                    } else {
-                        REMOTE
-                    }
+    fn distance(&self, addr: &str) -> u16 {
+        let b_class = self.b_class(addr);
+        if b_class == self.local_b_class {
+            return 0;
+        }
+        if let Some(zone) = self.zones.get(b_class) {
+            if let Some(distances) = self.distances.get(&self.local_zone) {
+                if let Some(d) = distances.get(zone) {
+                    return *d;
                 }
             }
         }
+        std::u16::MAX
     }
     // 获取B网段
     fn b_class<'a>(&self, addr: &'a str) -> &'a str {
@@ -159,21 +163,34 @@ impl DistanceCalculator {
             Ok(cfg) => {
                 let mut zones = HashMap::with_capacity(64);
                 for one in cfg.relation.iter() {
-                    let fields: Vec<&str> = one.split(|c| c == ',' || c == '-').collect();
-                    if fields.len() == 4 {
+                    let fields: Vec<&str> = one.split(",").collect();
+                    if fields.len() == 3 {
                         let addr_b_class = fields[0].trim().to_string();
-                        let city = fields[1].trim().to_string();
-                        let zone = fields[2].trim().to_string();
-                        zones.insert(addr_b_class, (city, zone));
+                        let zone = fields[1].trim().to_string();
+                        // let idc = fields[2].trim().to_string();
+                        zones.insert(addr_b_class, zone);
                     }
                 }
                 self.zones = zones;
-                let local_addr = self.b_class(metrics::raw_local_ip());
-                if let Some((city, zone)) = self.zones.get(local_addr) {
-                    self.local_city = city.to_string();
+                self.local_b_class = self.b_class(metrics::raw_local_ip()).to_string();
+                if let Some(zone) = self.zones.get(&self.local_b_class) {
                     self.local_zone = zone.to_string();
                 }
-                log::debug!("{} zones:{:?}", local_addr, self);
+
+                // 通过优先级计算不同zone之间的距离
+                let cap = cfg.priority.len();
+                let mut distances = HashMap::with_capacity(cap);
+                for one in cfg.priority {
+                    let zones: Vec<&str> = one.split(",").collect();
+                    let mut d = HashMap::with_capacity(cap);
+                    for (i, z) in zones.iter().enumerate() {
+                        // 同个B段是距离是0，同一个ZONE距离是1
+                        d.insert(z.to_string(), 1 + i as u16);
+                    }
+                    distances.insert(zones[0].to_string(), d);
+                }
+                self.distances = distances;
+                log::info!("zones:{:?}", self);
             }
         };
     }
@@ -199,6 +216,8 @@ fn refresh_idc(cfg: &str) {
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 pub struct IdcRegionCfg {
+    #[serde(default)]
+    priority: Vec<String>,
     #[serde(default)]
     relation: Vec<String>,
 }
