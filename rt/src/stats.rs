a699ec4753582889a34357ddb32fd2c9e8af58d6
icycrystal4 <icycrystal4@gmail.com>
2022-01-09 10:31:28 +0000

    timeout: 把超时控制逻辑抽象至runtime模块中，方便统一处理

--- rt/src/stats.rs
+++ rt/src/stats.rs
@@ -50,11 +50,17 @@ impl<F: Future<Output = Result<()>> + ReEnter + Debug + Unpin> Future for Timeou
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let (tx, rx) = (self.inner.num_tx(), self.inner.num_rx());
         if tx > rx {
-            let elapsed = self.last.elapsed();
-            if elapsed >= Duration::from_millis(20) {
-                log::info!("reenter {:?} {} > {} => {:?}", elapsed, tx, rx, self.inner);
+            if self.last.elapsed() >= Duration::from_millis(20) {
+                log::info!(
+                    "reenter {:?} {} > {} => {:?}",
+                    self.last.elapsed(),
+                    tx,
+                    rx,
+                    self.inner
+                );
             }
-            if elapsed >= self.timeout {
+            if self.last_rx.elapsed() >= self.timeout {
+                let elapsed = self.last_rx.elapsed();
                 log::info!(
                     "receive no response after {:?}. tx:{} rx:{} => {:?}",
                     elapsed,
@@ -64,11 +70,14 @@ impl<F: Future<Output = Result<()>> + ReEnter + Debug + Unpin> Future for Timeou
                 );
                 return Poll::Ready(Err(protocol::Error::Timeout(elapsed)));
             }
+        } else {
+            self.last_rx = Instant::now();
         }
         let ret = Pin::new(&mut self.inner).poll(cx);
         let (tx_post, rx_post) = (self.inner.num_tx(), self.inner.num_rx());
         if tx_post > rx_post {
             self.last = Instant::now();
+            // 有接收到请求，则更新timeout基准
             if rx_post > rx {
                 self.last_rx = self.last;
             }
