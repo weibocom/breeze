1492de9270c23a9562f7ae7312f81536e7ef3271
icycrystal4 <icycrystal4@gmail.com>
2022-01-10 11:37:07 +0000

    redis: 如果只有一个从，则允许在访问失败时，访问主

--- endpoint/src/redisservice/mod.rs
+++ endpoint/src/redisservice/mod.rs
@@ -1,2 +1,13 @@
 pub(super) mod config;
 pub mod topo;
+
+struct Context {
+    runs: u32, // 运行的次数
+    idx: u32,
+}
+
+#[inline(always)]
+fn transmute(ctx: &mut u64) -> &mut Context {
+    debug_assert_eq!(std::mem::size_of::<Context>(), 8);
+    unsafe { std::mem::transmute(ctx) }
+}
