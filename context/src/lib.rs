f85dbc8d1f63711e28a7b94a89769d71c02c6084
icycrystal4 <icycrystal4@gmail.com>
2022-02-14 06:21:34 +0000

    git version

--- context/src/lib.rs
+++ context/src/lib.rs
@@ -1,4 +1,6 @@
+extern crate lazy_static;
 use clap::{FromArgMatches, IntoApp, Parser};
+use lazy_static::lazy_static;
 use std::io::{Error, ErrorKind, Result};
 use std::path::Path;
 use url::Url;
@@ -76,16 +78,22 @@ pub struct Context {
     service_pool: String,
 }
 
-static VERSION: &'static str = git_version::git_version!();
+const VERSION: &'static str = git_version::git_version!();
+lazy_static! {
+    static ref SHORT_VERSION: &'static str = {
+        let mut idx = VERSION.rfind('-').unwrap_or(0);
+        if &VERSION[idx..] == "-modified" && idx > 0 {
+            idx = VERSION[0..idx].rfind('-').unwrap_or(0);
+        }
+        if idx < VERSION.len() && VERSION.as_bytes()[idx] == b'-' {
+            idx += 1
+        }
+        &VERSION[idx..]
+    };
+}
+#[inline(always)]
 pub fn get_short_version() -> &'static str {
-    let mut idx = VERSION.rfind('-').unwrap_or(0);
-    if &VERSION[idx..] == "-modified" && idx > 0 {
-        idx = VERSION[0..idx].rfind('-').unwrap_or(0);
-    }
-    if idx < VERSION.len() && VERSION.as_bytes()[idx] == b'-' {
-        idx += 1
-    }
-    &VERSION[idx..]
+    &SHORT_VERSION
 }
 
 impl Context {
