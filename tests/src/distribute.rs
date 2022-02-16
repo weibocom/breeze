f69dc5aa99217f02d48f8694ece3e75c849d8c75
陈波 <chenbo@staff.weibo.com>
2022-02-16 17:16:00 +0800

    support new distribution: splitmod

--- tests/src/distribute.rs
+++ tests/src/distribute.rs
@@ -36,4 +36,20 @@ mod distribute_test {
         let idx = dist.index(hash);
         println!("key:{}, hash:{}, idx: {}", key, hash, idx);
     }
+
+    #[test]
+    fn splitmod() {
+        let shards_count = 16;
+        let mut shards = Vec::with_capacity(shards_count);
+        let hasher = Hasher::from("crc32-range-id");
+        for i in 0..shards_count {
+            shards.push(format!("192.168.10.{}", i));
+        }
+        let dist = Distribute::from("splitmod-32", &shards);
+
+        let key = "1234567890.fri";
+        let hash = hasher.hash(&key.as_bytes());
+        let idx = dist.index(hash);
+        println!("key:{}, hash:{}, idx: {}", key, hash, idx);
+    }
 }
