10871bafc984449040792b3cd588bc45c6fe76ee
icycrystal4 <icycrystal4@gmail.com>
2022-02-27 08:55:56 +0000

    main: 调整tokio::runtime的构建方式

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -16,9 +16,74 @@ use std::panic;
 
 use backtrace::Backtrace;
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
-//#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
+    let ctx = Context::from_os_args();
+    ctx.check()?;
+    set_rlimit(ctx.no_file);
+    set_panic_hook();
+
+    let threads = ctx.thread_num as usize;
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(threads)
+        .thread_name("breeze")
+        .thread_stack_size(2 * 1024 * 1024)
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async { run(ctx).await })
+}
+
+async fn run(ctx: Context) -> Result<()> {
+    let _l = service::listener_for_supervisor(ctx.port()).await?;
+    elog::init(ctx.log_dir(), &ctx.log_level)?;
+
+    // 在专用线程中初始化4个定时任务。
+    metrics::init_local_ip(&ctx.metrics_probe);
+    let mut spawner = rt::DedicatedSpawner::new();
+    spawner.spawn(metrics::Sender::new(
+        &ctx.metrics_url(),
+        &ctx.service_pool(),
+        Duration::from_secs(10),
+    ));
+    spawner.spawn(metrics::MetricRegister::default());
+    spawner.spawn(discovery::dns::start_dns_resolver_refresher());
+    let discovery = discovery::Discovery::from_url(ctx.discovery());
+    let (tx, rx) = ds::chan::bounded(128);
+    let snapshot = ctx.snapshot().to_string();
+    let tick = ctx.tick();
+    let mut fix = discovery::Fixed::default();
+    fix.register(ctx.idc_path(), sharding::build_refresh_idc());
+    spawner.spawn(watch_discovery(snapshot, discovery, rx, tick, fix));
+    //spawner.spawn(stream::start_delay_drop());
+    spawner.start_on_dedicated_thread();
+
+    // 启动定期更新资源配置线程
+    // 部分资源需要延迟drop。
+
+    log::info!("====> server inited <==== {:?}", ctx);
+
+    let mut listeners = ctx.listeners();
+    loop {
+        for quard in listeners.scan().await {
+            let discovery = tx.clone();
+            spawn(async move {
+                match service::process_one(&quard, discovery).await {
+                    Ok(_) => log::info!("service complete:{}", quard),
+                    Err(e) => log::warn!("service failed. {} err:{:?}", quard, e),
+                }
+            });
+        }
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+fn set_rlimit(no: u64) {
+    // set number of file
+    if let Err(e) = rlimit::setrlimit(rlimit::Resource::NOFILE, no, no) {
+        log::info!("set rlimit to {} failed:{:?}", no, e);
+    }
+}
+fn set_panic_hook() {
     panic::set_hook(Box::new(|panic_info| {
         let (filename, line) = panic_info
             .location()
@@ -41,54 +106,4 @@ async fn main() -> Result<()> {
         log::error!("A panic occurred at {}:{}: {}", filename, line, cause);
         log::error!("panic backtrace: {:?}", Backtrace::new())
     }));
-    let ctx = Context::from_os_args();
-    ctx.check()?;
-    // set number of file
-    if let Err(e) = rlimit::setrlimit(rlimit::Resource::NOFILE, ctx.no_file, ctx.no_file) {
-        log::info!("set rlimit to {} failed:{:?}", ctx.no_file, e);
-    }
-
-    let _l = service::listener_for_supervisor(ctx.port()).await?;
-    elog::init(ctx.log_dir(), &ctx.log_level)?;
-
-    // 在专用线程中初始化4个定时任务。
-    metrics::init_local_ip(&ctx.metrics_probe);
-    let mut spawner = rt::DedicatedSpawner::new();
-    spawner.spawn(metrics::Sender::new(
-        &ctx.metrics_url(),
-        &ctx.service_pool(),
-        Duration::from_secs(10),
-    ));
-    spawner.spawn(metrics::MetricRegister::default());
-    spawner.spawn(discovery::dns::start_dns_resolver_refresher());
-    use discovery::watch_discovery;
-    let discovery = Discovery::from_url(ctx.discovery());
-    let (tx, rx) = ds::chan::bounded(128);
-    let snapshot = ctx.snapshot().to_string();
-    let tick = ctx.tick();
-    let mut fix = discovery::Fixed::default();
-    fix.register(ctx.idc_path(), sharding::build_refresh_idc());
-    spawner.spawn(watch_discovery(snapshot, discovery, rx, tick, fix));
-    //spawner.spawn(stream::start_delay_drop());
-    log::info!("starting a dedicated thread for periodic tasks");
-    spawner.start_on_dedicated_thread();
-
-    // 启动定期更新资源配置线程
-    // 部分资源需要延迟drop。
-
-    log::info!("====> server inited <====");
-
-    let mut listeners = ctx.listeners();
-    loop {
-        for quard in listeners.scan().await {
-            let discovery = tx.clone();
-            spawn(async move {
-                match service::process_one(&quard, discovery).await {
-                    Ok(_) => log::info!("service complete:{}", quard),
-                    Err(e) => log::warn!("service failed. {} err:{:?}", quard, e),
-                }
-            });
-        }
-        tokio::time::sleep(Duration::from_secs(1)).await;
-    }
 }
