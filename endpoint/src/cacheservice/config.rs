a08c3a34a7d11eccb33661b5ccb95c842b557a3c
xinxin22 <xinxin22@staff.weibo.com>
2022-03-07 16:30:03 +0800

    超时更改为80/200

--- endpoint/src/cacheservice/config.rs
+++ endpoint/src/cacheservice/config.rs
@@ -45,10 +45,10 @@ impl Namespace {
         }
     }
     pub(super) fn timeout_master(&self) -> Duration {
-        Duration::from_millis(250.max(self.timeout_ms_master as u64))
+        Duration::from_millis(200.max(self.timeout_ms_master as u64))
     }
     pub(super) fn timeout_slave(&self) -> Duration {
-        Duration::from_millis(100.max(self.timeout_ms_slave as u64))
+        Duration::from_millis(80.max(self.timeout_ms_slave as u64))
     }
 }
 
