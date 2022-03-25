c92e6047659f2469859e0fdf47eb503c4863dc70
陈波 <chenbo@staff.weibo.com>
2022-03-25 14:29:45 +0800

    support new distribution: slotmod

--- tests/src/distribute.rs
+++ tests/src/distribute.rs
@@ -52,4 +52,20 @@ mod distribute_test {
         let idx = dist.index(hash);
         println!("key:{}, hash:{}, idx: {}", key, hash, idx);
     }
+
+    #[test]
+    fn slotmod() {
+        let shards_count = 4;
+        let mut shards = Vec::with_capacity(shards_count);
+        let hasher = Hasher::from("crc32");
+        for i in 0..shards_count {
+            shards.push(format!("192.168.10.{}", i));
+        }
+        let dist = Distribute::from("slotmod-1024", &shards);
+
+        let key = "1234567890.fri";
+        let hash = hasher.hash(&key.as_bytes());
+        let idx = dist.index(hash);
+        println!("key:{}, hash:{}, idx: {}", key, hash, idx);
+    }
 }
