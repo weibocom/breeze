d303c29cfef43c1ec96141903715ed8d85fbbe74
陈波 <chenbo@staff.weibo.com>
2022-05-26 15:58:23 +0800

    update max buf to 64m

--- endpoint/src/redisservice/topo.rs
+++ endpoint/src/redisservice/topo.rs
@@ -73,9 +73,9 @@ where
         assert!(shard_idx < self.shards.len(), "{:?}", req);
         let shard = unsafe { self.shards.get_unchecked(shard_idx) };
 
-        // 跟踪hash为0的场景，hash设置错误、潜在bug可能导致hash为0，待2022.12后再考虑清理 fishermen
+        // 跟踪hash<=0的场景，hash设置错误、潜在bug可能导致hash为0，特殊场景hash可能为负，待2022.12后再考虑清理 fishermen
         use protocol::Utf8;
-        if req.hash() == 0 {
+        if req.hash() <= 0 {
             log::warn!(
                 "+++ careful - {} hash/idx:{}/{}, req:{:?}",
                 self.service,
@@ -84,13 +84,6 @@ where
                 req.data().utf8()
             )
         }
-        log::debug!(
-            "+++ - {} hash/idx:{}/{}, req:{:?}",
-            self.service,
-            req.hash(),
-            shard_idx,
-            req.data().utf8()
-        );
 
         // 如果有从，并且是读请求，如果目标server异常，会重试其他slave节点
         if shard.has_slave() && !req.operation().is_store() {
