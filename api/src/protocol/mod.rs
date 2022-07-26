2af31103ed33d0b727165f8a7ab6cf638c721dd9
陈波 <chenbo@staff.weibo.com>
2022-07-26 11:00:30 +0800

    支持api访问mc二进制协议

--- api/src/protocol/mod.rs
+++ api/src/protocol/mod.rs
@@ -10,5 +10,6 @@ pub const API_BASE: &str = "/breeze";
 // redis 增加api路由接口
 pub fn routes(rocket: Rocket<Build>) -> Rocket<Build> {
     let rocket = redis::routes(rocket);
-    phantom::routes(rocket)
+    let rocket = phantom::routes(rocket);
+    memcache::routes(rocket)
 }
