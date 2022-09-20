c7cb472452255d483566077ab07b596aeca469f7
icycrystal4@gmail.com <icycrystal4>
2022-09-20 14:39:09 +0800

    cow: update时，取消spin, 并且只将old放入dropping列表。在最后一个ReadGuard释放时，说明没有对old的引用，触发old 列表安全drop

--- ds/src/cow/read.rs
+++ ds/src/cow/read.rs
@@ -1,5 +1,8 @@
 use std::ptr::NonNull;
-use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicUsize, Ordering};
+use std::sync::atomic::{
+    AtomicBool, AtomicPtr, AtomicUsize,
+    Ordering::{AcqRel, Acquire, Release},
+};
 use std::sync::Arc;
 #[derive(Clone)]
 pub struct CowReadHandle<T> {
@@ -22,6 +25,7 @@ impl<T> From<T> for CowReadHandle<T> {
 
 impl<T> std::ops::Deref for CowReadHandle<T> {
     type Target = CowReadHandleInner<T>;
+    #[inline]
     fn deref(&self) -> &Self::Target {
         &self.inner
     }
@@ -29,23 +33,23 @@ impl<T> std::ops::Deref for CowReadHandle<T> {
 pub struct CowReadHandleInner<T> {
     inner: AtomicPtr<T>,
     enters: AtomicUsize,
-    pub(crate) epoch: AtomicBool,
+    pub(super) epoch: AtomicBool,
     // 先次更新完之后，会把正在处理中的数据存储到dropping中。所有的reader的读请求都迁移到inner之后，就可以安全的删除
-    dropping: AtomicPtr<T>,
+    dropping: AtomicPtr<Vec<*mut T>>,
     _t: std::marker::PhantomData<T>,
 }
 pub struct ReadGuard<'rh, T> {
-    inner: &'rh CowReadHandle<T>,
+    inner: &'rh CowReadHandleInner<T>,
 }
 impl<'rh, T> std::ops::Deref for ReadGuard<'rh, T> {
     type Target = T;
+    #[inline]
     fn deref(&self) -> &Self::Target {
         unsafe {
             &self
                 .inner
                 .inner
-                .inner
-                .load(Ordering::Acquire)
+                .load(Acquire)
                 .as_ref()
                 .expect("pointer is nil")
         }
@@ -55,12 +59,13 @@ impl<'rh, T> std::ops::Deref for ReadGuard<'rh, T> {
 impl<'rh, T> Drop for ReadGuard<'rh, T> {
     fn drop(&mut self) {
         // 删除dropping
-        if self.inner.enters.fetch_sub(1, Ordering::AcqRel) == 1 {
-            self.inner.take();
+        //println!("drop read guard");
+        if self.inner.enters.fetch_sub(1, AcqRel) == 1 {
+            self.inner.purge();
         }
     }
 }
-impl<T> CowReadHandle<T> {
+impl<T> CowReadHandleInner<T> {
     pub fn read<F: Fn(&T) -> R, R>(&self, f: F) -> R {
         f(&self.enter())
     }
@@ -69,30 +74,58 @@ impl<T> CowReadHandle<T> {
         self.enter()
     }
     fn enter(&self) -> ReadGuard<'_, T> {
-        self.enters.fetch_add(1, Ordering::AcqRel);
+        self.enters.fetch_add(1, AcqRel);
         ReadGuard { inner: self }
     }
     // 先把原有的数据swap出来，存储到dropping中。所有的reader请求都迁移到inner之后，将dropping中的数据删除。
-    // 在ReadGuard中处理
-    pub(crate) fn update(&self, t: T) {
-        assert!(self.epoch.load(Ordering::Acquire));
+    // 只在WriteHandler中调用。
+    pub(super) fn update(&self, t: T) {
+        assert!(self.epoch.load(Acquire));
+        // 确保安全, 确保在epoch设置为false之前，不会调用purge
         let w_handle = unsafe { NonNull::new_unchecked(Box::into_raw(Box::new(t))) };
-        let old = self.inner.inner.swap(w_handle.as_ptr(), Ordering::Release);
-        let dropped = self.dropping.swap(old, Ordering::Release);
-        self.drop_old(dropped);
-        // 确保安全
-        let guard = self.enter();
-        self.epoch.store(false, Ordering::Release);
-        drop(guard);
+        let old = self.inner.swap(w_handle.as_ptr(), Release);
+        unsafe { self.dropping(old) };
+    }
+    // 用swap来解决并发问题。
+    // 1. 先用0把pre swap出来；
+    // 2. 把new 与 pre进行合并
+    // 3. 再次进行swap，如果swap出来的数据如果不是zero，说明有并发问题。则继续
+    unsafe fn dropping(&self, old: *mut T) {
+        let mut new = Box::new(vec![old]);
+        loop {
+            //println!("dropping old:{:?}", old);
+            let pre = self.dropping.swap(0 as _, Release);
+            if !pre.is_null() {
+                let v: Vec<_> = *Box::from_raw(pre);
+                for d in v {
+                    if !new.contains(&d) {
+                        new.push(d);
+                    }
+                }
+            }
+            let zero = self.dropping.swap(Box::leak(new) as _, Release);
+            if zero.is_null() {
+                break;
+            }
+            //println!("dropping zero:{:?}", zero);
+            new = Box::from_raw(zero);
+            std::hint::spin_loop();
+        }
     }
     // 把dropping的数据转换出来，并且删除
-    fn take(&self) {
-        let empty = 0 as *mut T;
-        let old = self.dropping.swap(empty, Ordering::Release);
-        self.drop_old(old);
+    fn purge(&self) {
+        //println!("purge");
+        let old = self.dropping.swap(0 as _, Release);
+        if !old.is_null() {
+            let v: Vec<_> = unsafe { *Box::from_raw(old) };
+            for dropping in v {
+                self.drop_old(dropping);
+            }
+        }
     }
     fn drop_old(&self, p: *mut T) {
         if !p.is_null() {
+            //println!("drop old");
             let dropping = { unsafe { Box::from_raw(p) } };
             let t: T = *dropping;
             drop(t);
@@ -103,8 +136,9 @@ impl<T> CowReadHandle<T> {
 impl<T> Drop for CowReadHandleInner<T> {
     fn drop(&mut self) {
         unsafe {
-            let _dropping = Box::from_raw(self.dropping.load(Ordering::Acquire));
-            let _dropping = Box::from_raw(self.inner.load(Ordering::Acquire));
+            self.purge();
+            let _dropping = Box::from_raw(self.inner.load(Acquire));
+            drop(_dropping);
         }
     }
 }
--- ds/src/cow/write.rs
+++ ds/src/cow/write.rs
@@ -1,48 +1,43 @@
 use super::CowReadHandle;
-use std::sync::atomic::Ordering;
+use std::sync::atomic::Ordering::{AcqRel, Acquire};
 pub struct CowWriteHandle<T> {
     r_handle: CowReadHandle<T>,
 }
-impl<T> CowWriteHandle<T> {
+impl<T: Clone> CowWriteHandle<T> {
     pub(crate) fn from(r_handle: CowReadHandle<T>) -> Self {
         Self { r_handle }
     }
     // 如果上一次write请求还未结束，则进入spin状态.
-    pub fn write<F: FnMut(&mut T)>(&mut self, mut f: F)
-    where
-        T: Clone,
-    {
-        // 待上一次write请求结束
-        let mut spin = 0usize;
-        while let Err(_) =
-            self.r_handle
-                .epoch
-                .compare_exchange(false, true, Ordering::AcqRel, Ordering::Relaxed)
-        {
-            spin += 1;
-            std::hint::spin_loop();
-        }
-        //  fence(Ordering::Acquire);
-        if spin > 0 {
-            log::info!("cow spins {} times.", spin);
-        }
+    pub fn write<F: FnMut(&mut T)>(&mut self, mut f: F) {
         let mut t: T = self.r_handle.read(|t| t.clone());
         f(&mut t);
+        self.update(t);
+    }
+    #[inline]
+    pub fn update(&mut self, t: T) {
+        // lock
+        self.r_handle
+            .epoch
+            .compare_exchange(false, true, AcqRel, Acquire)
+            .expect("lock failed");
+
+        let guard = self.get();
+        // 在r_handle.update中更新epoch为false.
         self.r_handle.update(t);
+
+        self.r_handle
+            .epoch
+            .compare_exchange(true, false, AcqRel, Acquire)
+            .expect("unlock failed");
+
+        drop(guard);
     }
-    pub fn update(&mut self, t: T)
-    where
-        T: Clone,
-    {
-        self.write(move |p| *p = t.clone());
-    }
-    pub fn copy(&self) -> T
-    where
-        T: Clone,
-    {
+    #[inline]
+    pub fn copy(&self) -> T {
         self.r_handle.read(|t| t.clone())
     }
-    pub fn get(&self) -> crate::ReadGuard<'_, T> {
+    #[inline]
+    pub fn get(&self) -> super::ReadGuard<'_, T> {
         self.r_handle.get()
     }
 }
