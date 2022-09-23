dbe6a96593c4244c67c6139b21a24999889257ae
icycrystal4@gmail.com <icycrystal4>
2022-09-23 14:49:39 +0800

    http: 添加压缩支持

--- agent/src/http.rs
+++ agent/src/http.rs
@@ -1,22 +1,16 @@
-cfg_if::cfg_if! {
-if #[cfg(feature = "http")] {
-
 // 必须运行在tokio的runtime环境中
 pub(super) fn start_http_server(ctx: &context::Context, rt: &tokio::runtime::Runtime) {
-    log::info!("starting http server!");
-    let rocket = rocket::build();
-    let rocket = crate::console::init_routes(rocket, ctx, rt);
-    let rocket = crate::prometheus::init_routes(rocket);
-    rt.spawn(async {
-        if let Err(_e) = rocket.launch().await {
-            log::error!("launch rocket failed: {:?}", _e);
-        };
-    });
-}
-
-
-} else {
-pub(super) fn start_http_server(_ctx: &context::Context, _rt: &tokio::runtime::Runtime) {}
-}
-
+    if cfg!(feature = "http") {
+        log::info!("starting http server!");
+        let rocket = rocket::build();
+        let rocket = crate::console::init_routes(rocket, ctx, rt);
+        let rocket = crate::prometheus::init_routes(rocket);
+        use rocket_async_compression::Compression;
+        let rocket = rocket.attach(Compression::fairing());
+        rt.spawn(async {
+            if let Err(_e) = rocket.launch().await {
+                log::error!("launch rocket failed: {:?}", _e);
+            };
+        });
+    }
 }
