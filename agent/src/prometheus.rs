1183a980e07dd2a70f5ae43f14afc2863e723f01
icycrystal4@gmail.com <icycrystal4>
2022-09-23 14:50:01 +0800

    调整部分编码规范

--- agent/src/prometheus.rs
+++ agent/src/prometheus.rs
@@ -1,56 +1,56 @@
-cfg_if::cfg_if! {
-if #[cfg(feature = "http")] {
+pub use inner::*;
+#[cfg(feature = "http")]
+mod inner {
+    use metrics::prometheus::Prometheus;
+    use rocket::{
+        http::Header,
+        request::Request,
+        response::{stream::ReaderStream, Responder, Response},
+        Build, Rocket,
+    };
 
-use metrics::prometheus::Prometheus;
-use rocket::{
-    request::Request,
-    response::{stream::ReaderStream, Responder, Response},
-    Build, Rocket,
-    http::Header,
-};
+    pub(crate) fn init_routes(rocket: Rocket<Build>) -> Rocket<Build> {
+        rocket.mount("/metrics", routes![prometheus_metrics])
+    }
 
-pub(crate) fn init_routes(rocket: Rocket<Build>) -> Rocket<Build> {
-    rocket.mount("/metrics", routes![prometheus_metrics])
-}
+    #[get("/")]
+    fn prometheus_metrics() -> PrometheusMetricsResponse {
+        PrometheusMetricsResponse {}
+    }
 
-#[get("/")]
-fn prometheus_metrics() -> PrometheusMetricsResponse {
-    PrometheusMetricsResponse {}
-}
+    pub struct PrometheusMetricsResponse {}
 
-pub struct PrometheusMetricsResponse {}
+    use ds::lock::Lock;
+    use lazy_static::lazy_static;
+    use std::time::Instant;
 
-use std::time::{Instant};
-use ds::lock::Lock;
-use lazy_static::lazy_static;
+    lazy_static! {
+        static ref LAST: Lock<Instant> = Instant::now().into();
+    }
 
-lazy_static! {
-static ref LAST:Lock<Instant> = Instant::now().into();
-}
-
-impl<'r> Responder<'r, 'r> for PrometheusMetricsResponse {
-    fn respond_to(self, _: &Request) -> rocket::response::Result<'r> {
-        let mut response = Response::build();
-        if let Ok(mut last) = LAST.try_lock() {
-            let secs = last.elapsed().as_secs_f64();
-            if secs >= 8f64 {
-                *last = Instant::now();
-                let metrics = Prometheus::new(secs);
-                let stream: ReaderStream<Prometheus> = metrics.into();
-                return response.streamed_body(stream).ok()
+    impl<'r> Responder<'r, 'r> for PrometheusMetricsResponse {
+        fn respond_to(self, _: &Request) -> rocket::response::Result<'r> {
+            let mut response = Response::build();
+            if let Ok(mut last) = LAST.try_lock() {
+                let secs = last.elapsed().as_secs_f64();
+                if secs >= 8f64 {
+                    *last = Instant::now();
+                    let metrics = Prometheus::new(secs);
+                    let stream: ReaderStream<Prometheus> = metrics.into();
+                    return response.streamed_body(stream).ok();
+                }
+                return response
+                    .header(Header::new("too-frequently", secs.to_string()))
+                    .ok();
             }
-            return response.header(Header::new("too-frequently", secs.to_string())).ok()
+            response.header(Header::new("lock", "failed")).ok()
         }
-        response.header(Header::new("lock", "failed")).ok()
     }
 }
-
-
-
-} else {
-pub(super) fn init_routes(rocket: Rocket<Build>) -> Rocket<Build> {
-    rocket
-}
-}
-
+#[cfg(not(feature = "http"))]
+mod inner {
+    use rocket::{Build, Rocket};
+    pub(crate) fn init_routes(rocket: Rocket<Build>) -> Rocket<Build> {
+        rocket
+    }
 }
--- log/src/lib.rs
+++ log/src/lib.rs
@@ -7,10 +7,10 @@ cfg_if::cfg_if! {
     } else {
         mod disable;
         pub use disable::*;
+        use std::io::Write;
         pub fn init(path: &str, _l: &str) -> std::io::Result<()> {
             std::fs::create_dir_all(path)?;
             let mut log = std::fs::File::create(format!("{}/breeze.log", path))?;
-            use std::io::Write;
             log.write(b"===> log disabled <===")?;
             Ok(())
         }
