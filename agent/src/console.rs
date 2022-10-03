67006098dbdcf8889b2017e7190601ddb90193d7
icycrystal4@gmail.com <icycrystal4>
2022-10-03 16:20:42 +0800

    去除cfg-if的直接依赖

--- agent/src/console.rs
+++ agent/src/console.rs
@@ -1,30 +1,12 @@
 use context::Context;
 use rocket::{Build, Rocket};
 
-cfg_if::cfg_if! {
-if #[cfg(feature = "http")] {
-
-
 pub(crate) fn init_routes(
     rocket: Rocket<Build>,
     ctx: &Context,
     rt: &tokio::runtime::Runtime,
 ) -> Rocket<Build> {
+    #[cfg(feature = "http")]
     rt.spawn(api::start_whitelist_refresh(ctx.whitelist_host.clone()));
     rocket
-}
-
-} else {
-
-pub(crate) fn init_routes(
-    rocket: Rocket<Build>,
-    _ctx: &Context,
-    _rt: &tokio::runtime::Runtime,
-) -> Rocket<Build> {
-    rocket
-}
-
-
-}
-
 }
