3d89f69e54d918944cf6cf4a0ff68e912f7aaee9
wanggang9 <wanggang9@staff.weibo.com>
2022-10-08 14:28:19 +0800

    optimize

--- agent/src/console.rs
+++ agent/src/console.rs
@@ -1,14 +1,8 @@
 use context::Context;
 use rocket::{Build, Rocket};
 
-pub(crate) fn init_routes(
-    rocket: Rocket<Build>,
-    ctx: &Context,
-    rt: &tokio::runtime::Runtime,
-) -> Rocket<Build> {
-    #[cfg(feature = "http")]
-    rt.spawn(api::start_whitelist_refresh(ctx.whitelist_host.clone()));
-    #[cfg(feature = "console-api")]
+pub(crate) fn init_routes(rocket: Rocket<Build>, ctx: &Context) -> Rocket<Build> {
+    rt::spawn(api::start_whitelist_refresh(ctx.whitelist_host.clone()));
     let rocket = api::routes(rocket);
     rocket
 }
