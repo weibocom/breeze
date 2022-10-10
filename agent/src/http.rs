0a4f0e29bc9527b023509069abfe32816b027d2e
xiaowang <xiaowang@staff.weibo.com>
2022-10-10 19:04:28 +0800

    recoket整合到ctx

--- agent/src/http.rs
+++ agent/src/http.rs
@@ -1,8 +1,18 @@
+use rocket::config::{Config, Sig};
 // 必须运行在tokio的runtime环境中
 pub(super) fn start_http_server(ctx: &context::Context) {
     if cfg!(feature = "http") {
         log::info!("starting http server!");
-        let rocket = rocket::build();
+        let mut c = Config::default();
+        c.shutdown.ctrlc = false;
+        c.shutdown.signals.insert(Sig::Hup);
+        let config = Config::figment()
+            .merge(("address", "0.0.0.0"))
+            .merge(("port", &ctx.port))
+            .merge(("log_level", "critical"))
+            .merge(("workers", 4))
+            .merge(("shutdown", c.shutdown));
+        let rocket = rocket::custom(config);
         #[cfg(feature = "console-api")]
         let rocket = crate::console::init_routes(rocket, ctx);
         let rocket = crate::prometheus::init_routes(rocket);
