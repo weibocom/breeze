cc23ac3082ad49733490c051086f48e4d384c5f2
icycrystal4@gmail.com <icycrystal4>
2022-10-10 17:51:29 +0800

    删除不必要的代码

--- ds/src/futures.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! ready {
-    ($e:expr $(,)?) => {
-        match $e {
-            std::task::Poll::Ready(t) => t,
-            std::task::Poll::Pending => return std::task::Poll::Pending,
-        }
-    };
-}
