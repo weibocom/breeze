7725532e018ef3dd2ea6714448abf3ff0de4a6ed
icycrystal4 <icycrystal4@gmail.com>
2022-10-19 16:00:22 +0800

    prometheus: 注册target json格式修复

--- agent/src/prometheus.rs
+++ agent/src/prometheus.rs
@@ -64,9 +64,11 @@ pub(crate) fn register_target(ctx: &context::Context) {
     "pool": "{}",
     "job": "datamesh-agent"
   }},
-  "port": {}
+  "target": "{}:{}"
 }}"#,
-            pool, port
+            pool,
+            metrics::local_ip(),
+            port
         );
         let client = reqwest::Client::new();
         let mut interval = tokio::time::interval(std::time::Duration::from_secs(60));
