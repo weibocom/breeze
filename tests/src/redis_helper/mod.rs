e0b058ea38fba54244f7e75e8f849c6526dcf0d5
hailong15 <hailong15@staff.weibo.com>
2022-10-19 15:48:43 +0800

    add redis test

---
+++ tests/src/redis_helper/mod.rs
@@ -0,0 +1,13 @@
+use std::io::{Error, ErrorKind, Result};
+
+use redis::{Client, Connection};
+
+pub fn get_conn(host: &str) -> Connection {
+    let client = Client::open(host);
+    assert!(!client.is_err(), "get client err:{:?}", client.err());
+    let client = client.unwrap();
+
+    let conn = client.get_connection();
+    assert!(!conn.is_err(), "get conn err:{:?}", conn.err());
+    conn.unwrap()
+}
