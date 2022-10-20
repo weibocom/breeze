0dd72b9bf80739afae780a4baf145b7ba6c97afb
ficohu <flyinghyp@outlook.com>
2022-10-20 09:31:59 +0800

    更新mc测试端口

--- tests/src/mc.rs
+++ tests/src/mc.rs
@@ -5,19 +5,19 @@ mod mc_test {
 
     /// 测试场景：buffer扩容验证，同一个连接，同一个key, set不同大小的value
     /// 特征:    key；固定为"fooset"  value: 不同长度的String,每个字符内容为 ‘A’
-    /// 
+    ///
     /// 测试步骤：
     ///     <1> 建立连接，
     ///     <2> 乱序set，先set 1M的value,再乱序set其他大小的value
     ///     <3> 将set进去的value get出来，对比set进去的值与get出来的值；
     ///     <4> 由小到大分别set不同大小的value，从0开始递增，覆盖从4k->8k, 8k->32k，以及 1M(1048511 byte) 的场景，
     ///     <5> 重复步骤 <3>
-    /// 
+    ///
     #[test]
     fn mc_test_set() {
         let client = mc_get_conn();
         let key = "fooset";
-        let mut v_sizes = [1048507, 4, 4000, 40, 8000, 20000,0,400];
+        let mut v_sizes = [1048507, 4, 4000, 40, 8000, 20000, 0, 400];
         let mut result: String;
         for v_size in v_sizes {
             let val = vec![0x41; v_size];
@@ -25,10 +25,13 @@ mod mc_test {
                 .set(key, &String::from_utf8_lossy(&val).to_string(), 2)
                 .unwrap();
             result = client.get(key).unwrap();
-            println!("len is {}",String::from_utf8_lossy(&val).to_string().capacity());
+            println!(
+                "len is {}",
+                String::from_utf8_lossy(&val).to_string().capacity()
+            );
             assert_eq!(result, String::from_utf8_lossy(&val).to_string());
         }
-        v_sizes = [0, 4, 40, 400, 4000, 8000, 20000,1048507];
+        v_sizes = [0, 4, 40, 400, 4000, 8000, 20000, 1048507];
         for v_size in v_sizes {
             let val = vec![0x41; v_size];
             client
@@ -40,10 +43,9 @@ mod mc_test {
         println!("completed mc set test!");
     }
 
-   
-    fn mc_get_conn() -> MemcachedClient{
-        let client_rs =MemcachedClient::new(vec!["127.0.0.1:11211"], 5);
-        assert_eq!(true,client_rs.is_ok());
+    fn mc_get_conn() -> MemcachedClient {
+        let client_rs = MemcachedClient::new(vec!["127.0.0.1:9301"], 5);
+        assert_eq!(true, client_rs.is_ok());
         return client_rs.unwrap();
     }
 
