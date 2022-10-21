9f2f4e56cd58757a92011aa58393ae256b3d3c6d
hailong15 <hailong15@staff.weibo.com>
2022-10-21 16:20:45 +0800

    add test

--- tests_ci/src/redis_helper.rs
+++ tests_ci/src/redis_helper.rs
@@ -1,12 +1,6 @@
-#[cfg(test)]
 pub fn get_conn(host: &str) -> redis::Connection {
     use redis::Client;
     let host = String::from("redis://") + host;
-    let client = Client::open(host);
-    assert!(!client.is_err(), "get client err:{:?}", client.err());
-    let client = client.unwrap();
-
-    let conn = client.get_connection();
-    assert!(!conn.is_err(), "get conn err:{:?}", conn.err());
-    conn.unwrap()
+    let client = Client::open(host).expect("get client err");
+    client.get_connection().expect("get conn err")
 }
