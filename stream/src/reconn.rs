a60c86c345c5e31965cd0198ad70d0a250373725
陈波 <chenbo@staff.weibo.com>
2022-11-01 16:41:18 +0800

    连接的最大连续失败次数阀值设为1

--- stream/src/reconn.rs
+++ stream/src/reconn.rs
@@ -18,46 +18,23 @@ impl ReconnPolicy {
             continue_fails: 0,
         }
     }
-    // pub fn success(&mut self) {
-    //     self.errs = 0;
-    // }
+
     // 连接失败，为下一次连接做准备：sleep一段时间，避免无意义的重连
     pub async fn conn_failed(&mut self) {
-        // TODO： 测试review完毕，删除 fishermen
-        // self.conns += 1;
-        // if self.conns == 1 {
-        //     return;
-        // }
-        // self.errs = self.errs.wrapping_add(1);
         self.metric += 1;
         self.continue_fails += 1;
 
-        static MAX_FAILED: usize = 3;
+        static MAX_FAILED: usize = 1;
         let mut sleep_mills = 0;
         if self.continue_fails >= MAX_FAILED {
-            // 连续失败超过3次，master sleep 100ms，slave sleep 3000
+            // 连续失败超过$MAX_FAILED次，master sleep 100ms，slave sleep 3000
             if self.single.load(Acquire) {
                 sleep_mills = 100;
             } else {
                 sleep_mills = 3 * 1000;
             }
         };
-        // TODO： 测试review完毕，删除 fishermen
-        // let sleep = if self.single.load(Acquire) {
-        //     let mills = if self.continue_fails < MAX_FAILED {
-        //         0
-        //     } else {
-        //         100
-        //     };
-        //     Duration::from_millis(mills)
-        // } else {
-        //     let mills = if self.continue_fails < MAX_FAILED {
-        //         0
-        //     } else {
-        //         3 * 1000
-        //     };
-        //     Duration::from_millis(mills)
-        // };
+
         if self.continue_fails == 1 {
             log::info!(
                 "{}-th conn {} sleep:{} ",
