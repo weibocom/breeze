3af8a4179304bdfb212c5f26d1389a899958bb70
hailong15 <hailong15@staff.weibo.com>
2022-11-03 17:51:57 +0800

    shards test

--- tests_integration/src/redis_helper.rs
+++ tests_integration/src/redis_helper.rs
@@ -1,5 +1,5 @@
+use redis::Client;
 pub fn get_conn(host: &str) -> redis::Connection {
-    use redis::Client;
     let host = String::from("redis://") + host;
     let client = Client::open(host).expect("get client err");
     client.get_connection().expect("get conn err")
