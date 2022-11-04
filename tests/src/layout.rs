a2f1a6c617b63d03c624c7e085dceefb2b9a2058
icycrystal4 <icycrystal4@gmail.com>
2022-11-04 11:33:23 +0800

    添加部分struct的layout测试用例

--- tests/src/layout.rs
+++ tests/src/layout.rs
@@ -1,5 +1,25 @@
 // 不要轻易变更这里面的测试用例，除非你知道你在做什么。拉相关同学进行方案评审。
-use std::{mem::size_of, sync::atomic::AtomicU32};
+use std::{
+    mem::size_of,
+    sync::{atomic::AtomicU32, Arc},
+};
+
+use protocol::Parser;
+use stream::{Backend, Request};
+type Endpoint = Arc<Backend<Request>>;
+type Topology = endpoint::Topology<Builder, Endpoint, Request, Parser>;
+
+type Stream = rt::Stream<tokio::net::TcpStream>;
+
+type Handler<'r> = stream::handler::Handler<'r, Request, Parser, Stream>;
+
+type Builder = stream::Builder<Parser, Request>;
+type CacheService = endpoint::cacheservice::topo::CacheService<Builder, Endpoint, Request, Parser>;
+type RedisService = endpoint::redisservice::topo::RedisService<Builder, Endpoint, Request, Parser>;
+type PhantomService =
+    endpoint::phantomservice::topo::PhantomService<Builder, Endpoint, Request, Parser>;
+type MsgQue = endpoint::msgque::topo::MsgQue<Builder, Endpoint, Request, Parser>;
+
 #[test]
 fn check_layout() {
     assert_eq!(32, size_of::<ds::RingSlice>());
@@ -11,9 +31,21 @@ fn check_layout() {
     );
     assert_eq!(24, size_of::<protocol::Flag>());
     assert_eq!(1, size_of::<protocol::Resource>());
-    let guard_size = if cfg!(debug_assertions) { 240 } else { 200 };
+    let guard_size = if cfg!(debug_assertions) { 232 } else { 192 };
     assert_eq!(guard_size, size_of::<stream::buffer::StreamGuard>());
     assert_eq!(56, size_of::<ds::queue::PinnedQueue<AtomicU32>>());
     assert_eq!(16, size_of::<metrics::Metric>());
     assert_eq!(64, size_of::<metrics::Item>());
+    assert_eq!(1, size_of::<Parser>());
+    assert_eq!(48, size_of::<Backend<Request>>());
+    let stream_size = if cfg!(debug_assertions) { 248 } else { 200 };
+    assert_eq!(stream_size, size_of::<Stream>());
+    assert_eq!(560, size_of::<Handler<'static>>());
+    assert_eq!(0, size_of::<Builder>());
+
+    assert_eq!(400, size_of::<Topology>());
+    assert_eq!(96, size_of::<CacheService>());
+    assert_eq!(264, size_of::<RedisService>());
+    assert_eq!(192, size_of::<PhantomService>());
+    assert_eq!(392, size_of::<MsgQue>());
 }
