b09a2c62f54970dab9970f0815a9d0ba9c4342b6
陈波 <chenbo@staff.weibo.com>
2022-11-25 15:29:47 +0800

    fix conflicts

--- protocol/src/phantom/flag.rs
+++ protocol/src/phantom/flag.rs
@@ -6,23 +6,7 @@ const KEY_COUNT_BITS: u8 = 16;
 const KEY_COUNT_MASK: u64 = (1 << KEY_COUNT_BITS) - 1;
 // 32: 标识是否是第一个key
 const MKEY_FIRST_SHIFT: u8 = KEY_COUNT_SHIFT + KEY_COUNT_BITS;
-const MKEY_FIRST_BIT: u8 = 1;
-
-// TODO 去掉padding_rsp 测试完毕后清理
-// 33~35: 3bits 是 padding_rsp
-// const PADDING_RSP_SHIFT: u8 = MKEY_FIRST_SHIFT + MKEY_FIRST_BIT;
-// const PADDING_RSP_BITS: u8 = 3;
-// const PADDING_RSP_MASK: u64 = (1 << PADDING_RSP_BITS) - 1;
-
-// // 36~43 8bit
-// const META_LEN_SHIFT: u8 = PADDING_RSP_SHIFT + PADDING_RSP_BITS;
-// const META_LEN_BITS: u8 = 8;
-// const META_LEN_MASK: u64 = (1 << META_LEN_BITS) - 1;
-
-// token len 目前没有用，先注释掉 fishermen
-// const TOKEN_LEN_SHIFT: u8 = META_LEN_BITS + META_LEN_BITS;
-// const TOKEN_LEN_BITS: u8 = 8;
-// const TOKEN_LEN_MASK: u64 = (1 << TOKEN_LEN_BITS) - 1;
+const _MKEY_FIRST_BIT: u8 = 1;
 
 pub(super) trait RedisFlager {
     fn set_key_count(&mut self, cnt: u16);
@@ -31,10 +15,6 @@ pub(super) trait RedisFlager {
     fn mkey_first(&self) -> bool;
     // fn set_padding_rsp(&mut self, idx: u8);
     // fn padding_rsp(&self) -> u8;
-    // fn set_meta_len(&mut self, l: u8);
-    // fn meta_len(&self) -> u8;
-    // fn set_token_count(&mut self, c: u8);
-    // fn token_count(&self) -> u8;
 }
 
 #[inline]
@@ -76,23 +56,6 @@ impl RedisFlager for u64 {
     // fn padding_rsp(&self) -> u8 {
     //     get(self, PADDING_RSP_SHIFT, PADDING_RSP_MASK) as u8
     // }
-    // #[inline]
-    // fn set_meta_len(&mut self, l: u8) {
-    //     set(self, META_LEN_SHIFT, META_LEN_MASK, l as u64);
-    // }
-    // #[inline]
-    // fn meta_len(&self) -> u8 {
-    //     get(self, META_LEN_SHIFT, META_LEN_MASK) as u8
-    // }
-
-    // #[inline]
-    // fn set_token_count(&mut self, c: u8) {
-    //     set(self, TOKEN_LEN_SHIFT, TOKEN_LEN_MASK, c as u64);
-    // }
-    // #[inline]
-    // fn token_count(&self) -> u8 {
-    //     get(self, TOKEN_LEN_SHIFT, TOKEN_LEN_MASK) as u8
-    // }
 }
 impl RedisFlager for crate::Flag {
     #[inline]
@@ -118,22 +81,5 @@ impl RedisFlager for crate::Flag {
     // #[inline]
     // fn padding_rsp(&self) -> u8 {
     //     self.ext().padding_rsp()
-    // }
-    // #[inline]
-    // fn set_meta_len(&mut self, l: u8) {
-    //     self.ext_mut().set_meta_len(l);
-    // }
-    // #[inline]
-    // fn meta_len(&self) -> u8 {
-    //     self.ext().meta_len()
-    // }
-
-    // #[inline]
-    // fn set_token_count(&mut self, c: u8) {
-    //     self.ext_mut().set_token_count(c);
-    // }
-    // #[inline]
-    // fn token_count(&self) -> u8 {
-    //     self.ext().token_count()
     // }
 }
