9e5dce0d611d5e959e9c85e8a2f46c09706be748
icycrystal4 <icycrystal4@gmail.com>
2022-12-28 18:18:42 +0800

    benches: 增加ring_slice遍历的压测对比

--- tests/src/benches/criterion.rs
+++ tests/src/benches/criterion.rs
@@ -1,10 +1,10 @@
 use criterion::{criterion_group, criterion_main};
 mod heap;
+mod ring_slice;
 mod time;
 
-use heap::*;
-use time::*;
+criterion_group!(time, time::bench_instant, time::bench_duration);
+criterion_group!(heap, heap::bench_get_checked);
+criterion_group!(ring_slice, ring_slice::bench_iter);
 
-criterion_group!(time, bench_instant, bench_duration);
-criterion_group!(heap, bench_get_checked);
-criterion_main!(time, heap);
+criterion_main!(time, heap, ring_slice);
