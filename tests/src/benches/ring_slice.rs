89c3bec2a90b6b46719ca70389deae07297e3767
icycrystal4 <icycrystal4@gmail.com>
2022-12-28 18:18:42 +0800

    benches: 增加ring_slice遍历的压测对比

---
+++ tests/src/benches/ring_slice.rs
@@ -0,0 +1,31 @@
+use criterion::{black_box, Criterion};
+use ds::RingSlice;
+pub(super) fn bench_iter(c: &mut Criterion) {
+    let slice = (0..2048).into_iter().map(|x| x as u8).collect::<Vec<u8>>();
+    let len = slice.len();
+    let mut group = c.benchmark_group("ring_slice_iter");
+    let rs = RingSlice::from(slice.as_ptr(), slice.len(), 0, 2048);
+    group.bench_function("vec", |b| {
+        b.iter(|| {
+            black_box({
+                let mut t = 0;
+                for i in 0..len {
+                    t += unsafe { *slice.get_unchecked(i) as u64 };
+                }
+                t
+            });
+        });
+    });
+    group.bench_function("iter", |b| {
+        b.iter(|| {
+            black_box({
+                let mut t = 0u64;
+                for i in 0..rs.len() {
+                    t += rs.at(i) as u64
+                }
+                t
+            });
+        });
+    });
+    group.finish();
+}
