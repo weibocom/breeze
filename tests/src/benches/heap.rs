eb948fd7abded0f94d0a58b2684cdd8498792b47
icycrystal4 <icycrystal4@gmail.com>
2022-12-28 15:33:46 +0800

    benches: 添加vec get与unchecked_get的压测

--- tests/src/benches/heap.rs
+++ tests/src/benches/heap.rs
@@ -1,12 +1,13 @@
 use criterion::{black_box, Criterion};
 pub(super) fn bench_get_checked(c: &mut Criterion) {
-    let slice = [1u8; 2048];
+    let slice = (0..64).into_iter().map(|x| x as u8).collect::<Vec<u8>>();
+    let len = slice.len();
     let mut group = c.benchmark_group("vec_get");
     group.bench_function("checked", |b| {
         b.iter(|| {
             black_box({
                 let mut t = 0;
-                for i in 0..slice.len() {
+                for i in 0..len {
                     t += *slice.get(i).unwrap_or(&0) as u64;
                 }
                 t
@@ -17,7 +18,7 @@ pub(super) fn bench_get_checked(c: &mut Criterion) {
         b.iter(|| {
             black_box({
                 let mut t = 0u64;
-                for i in 0..slice.len() {
+                for i in 0..len {
                     t += unsafe { *slice.get_unchecked(i) as u64 };
                 }
                 t
@@ -40,7 +41,7 @@ pub(super) fn bench_get_checked(c: &mut Criterion) {
         b.iter(|| {
             black_box({
                 let mut t = 0u64;
-                for i in 0..slice.len() {
+                for i in 0..len {
                     t += unsafe { *ptr.add(i) as u64 };
                 }
                 t
