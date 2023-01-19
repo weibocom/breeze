3fe37e6a01be13294255a96b273c4864746e46b1
icycrystal4 <icycrystal4@gmail.com>
2023-01-19 11:35:32 +0800

    ring_slice: 提供基于宏的分段访问

--- tests/src/ring_slice.rs
+++ tests/src/ring_slice.rs
@@ -1,4 +1,5 @@
 use ds::RingSlice;
+use rand::Rng;
 #[test]
 fn test_ring_slice() {
     let cap = 1024;
@@ -12,6 +13,9 @@ fn test_ring_slice() {
     let in_range = RingSlice::from(ptr, cap, 0, 32);
     assert_eq!(in_range.len(), 32);
     assert_eq!(in_range.read(0), &dc[0..32]);
+    let (f, s) = in_range.data_oft(0);
+    assert_eq!(f, &dc[0..32]);
+    assert!(s.len() == 0);
 
     // 截止到末尾的
     let end_range = RingSlice::from(ptr, cap, cap - 32, cap);
@@ -19,6 +23,9 @@ fn test_ring_slice() {
     //assert_eq!(s.len(), 1);
     assert_eq!(end_range.len(), 32);
     assert_eq!(end_range.read(0), &dc[cap - 32..cap]);
+    let (f, s) = in_range.data_oft(0);
+    assert_eq!(f, &dc[0..32]);
+    assert!(s.len() == 0);
 
     let over_range = RingSlice::from(ptr, cap, cap - 32, cap + 32);
     //let s = over_range.as_slices();
@@ -30,6 +37,9 @@ fn test_ring_slice() {
     over_range.copy_to_vec(&mut v);
     assert_eq!(&v[0..32], &dc[cap - 32..cap]);
     assert_eq!(&v[32..], &dc[0..32]);
+    let (f, s) = over_range.data_oft(0);
+    assert_eq!(f, &dc[cap - 32..cap]);
+    assert_eq!(s, &dc[0..32]);
 
     let u32_num = 111234567u32;
     let bytes = u32_num.to_be_bytes();
@@ -62,64 +72,23 @@ fn test_ring_slice() {
 
     let _ = unsafe { Vec::from_raw_parts(ptr, 0, cap) };
 }
-//#[test]
-//fn test_ring_slice_map() {
-//    let slice_data = [98u8, 114, 101, 101, 122, 101, 45, 107, 101, 121, 45, 56, 57];
-//    let slice = Slice::from(&slice_data);
 
-//    // 第一个字节在最后
-//    let ring_slice_data: [u8; 16] = [
-//        114, 101, 101, 122, 101, 45, 107, 101, 121, 45, 56, 57, 0, 0, 0, 98,
-//    ];
-//    let ring_slice = RingSlice::from(ring_slice_data.as_ptr(), 16, 15, 15 + slice_data.len());
-
-//    assert_eq!(ring_slice, slice);
-//    let slice_to_ring: RingSlice = slice.clone().into();
-//    assert_eq!(ring_slice, slice_to_ring);
-
-//    //assert_eq!(hash(&slice_to_ring), hash(&ring_slice));
-
-//    let mut m = HashMap::with_capacity(4);
-//    m.insert(ring_slice, ());
-//    assert!(m.contains_key(&slice.into()));
-//}
-//use std::collections::hash_map::DefaultHasher;
-//use std::hash::{Hash, Hasher};
-//fn hash<T: Hash>(t: &T) -> u64 {
-//    let mut s = DefaultHasher::new();
-//    t.hash(&mut s);
-//    let h = s.finish();
-//    println!("hash ring:{}", h);
-//    h
-//}
-
-//#[test]
-//fn test_split_ring_slice() {
-//    println!("begin");
-
-//    let data = "STORED\r\n";
-//    let slice = RingSlice::from(data.as_ptr(), data.len(), 0, data.len());
-//    //let index = slice.find_sub(0, "sdfsfdssssd".as_ref());
-//    //println!("found = {}", index.is_some());
-
-//    /*
-//    let data = "END\r\nVALUE key1 0 9\r\nssksksksk\r\nVALUE key2 0 13\r\nabababababaab\r\n";
-//    let slice = RingSlice::from(data.as_ptr(), data.len(), 21, data.len() + 21);
-
-//    //let data = "VALUE key1 0 9\r\nssksksksk\r\nVALUE key2 0 13\r\nabababababaab\r\nEND\r\n";
-//    //let slice = RingSlice::from(data.as_ptr(), data.len(), 0, data.len());
-
-//    //let data = "VALUE key1 0 9\r\nssksksksk\r\nVALUE key2 0 13\r\nabababababaab\r\nEND";
-//    //let slice = RingSlice::from(data.as_ptr(), 64, 0, data.len());
-//    println!("slice generated");
-//    let index = slice.find_sub(0, "\r\n".as_ref());
-//    println!("found, index = {}", index.unwrap());
-//    let split = slice.split("\r\n".as_ref());
-//    println!("slice split, size = {}", split.len());
-//    for single in split {
-//        let mut single_vec: Vec<u8> = vec![];
-//        single.copy_to_vec(&mut single_vec);
-//        println!("single = {}", String::from_utf8(single_vec).unwrap());
-//    }
-//     */
-//}
+#[test]
+fn test_read_number() {
+    let cap = 1024;
+    let mut data: Vec<u8> = (0..cap)
+        .map(|_| rand::random::<u8>().max(b'a').min(b'z'))
+        .collect();
+    let ptr = data.as_mut_ptr();
+    std::mem::forget(data);
+    // 运行1000次，随机写入一个数
+    for _ in 0..1000 {
+        let start = rand::random::<u32>() as usize;
+        let len = (rand::random::<usize>() % cap).max(16);
+        let end = start + len;
+        let rs = RingSlice::from(ptr, cap, start, end);
+        let oft: usize = rand::thread_rng().gen_range(0..len - 8);
+        assert_eq!(rs.read_u64(oft), rs.read_num_be(oft));
+    }
+    let _ = unsafe { Vec::from_raw_parts(ptr, 0, cap) };
+}
