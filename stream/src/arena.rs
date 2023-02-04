b12252afb1fe751ebfcc3a7860e8877ba4ad9c77
icycrystal4 <icycrystal4@gmail.com>
2023-02-04 20:33:34 +0800

    mem arena: 自定义CallbackContext的Allocator. 支持local cache, global cache，与heap三级分配

---
+++ stream/src/arena.rs
@@ -0,0 +1,46 @@
+use std::ptr::NonNull;
+
+use ds::arena::{Allocator, Arena, Ephemera};
+
+use crate::CallbackContext;
+
+type L2Cache = GlobalArena;
+//type L2Cache = ds::arena::Heap;
+
+pub(crate) struct CallbackContextArena(Arena<CallbackContext, L2Cache>);
+
+// 首先从localcache分配；
+// 再从globalcache分配
+// 最后从默认的分配器分配
+impl CallbackContextArena {
+    pub(crate) fn with_cache(cache: usize) -> Self {
+        Self(Arena::with_cache(cache, L2Cache {}))
+    }
+    #[inline(always)]
+    pub(crate) fn alloc(&mut self, t: CallbackContext) -> NonNull<CallbackContext> {
+        self.0.alloc(t)
+    }
+    #[inline(always)]
+    pub(crate) fn dealloc(&mut self, ptr: NonNull<CallbackContext>) {
+        self.0.dealloc(ptr)
+    }
+}
+
+type Global = Ephemera<CallbackContext>;
+
+// 占用内存约为 20480 * 256 = 5.2M
+#[ctor::ctor]
+static GLOBAL_CACHE: Global = Global::with_cache(20480);
+
+struct GlobalArena;
+
+impl Allocator<CallbackContext> for GlobalArena {
+    #[inline(always)]
+    fn alloc(&self, t: CallbackContext) -> NonNull<CallbackContext> {
+        GLOBAL_CACHE.alloc(t)
+    }
+    #[inline(always)]
+    fn dealloc(&self, ptr: NonNull<CallbackContext>) {
+        GLOBAL_CACHE.dealloc(ptr)
+    }
+}
