7c94ad62c276e54bb70709ca8a631d03563ae592
kongkong <zhouthl@outlook.com>
2023-02-22 17:13:05 +0800

    优先使用idc配置的socklist

--- discovery/src/fixed.rs
+++ discovery/src/fixed.rs
@@ -2,24 +2,63 @@ use crate::Config;
 // 提供一个除了topology之外的，定期使用discovery的策略
 #[derive(Default)]
 pub struct Fixed {
-    // name, sig, callback
-    cbs: Vec<(String, String, Box<dyn Fn(&str) + 'static + Send + Sync>)>,
+    // name, idc, sig, callback
+    cbs: Vec<(
+        String,
+        &'static str,
+        String,
+        Box<dyn Fn(&str) + 'static + Send + Sync>,
+    )>,
 }
 
 impl Fixed {
     pub(crate) async fn with_discovery<D: crate::Discover>(&mut self, discovery: &D) {
-        for (name, sig, cb) in self.cbs.iter_mut() {
-            match discovery.get_service::<String>(name, sig).await {
-                Ok(Config::Config(new_sig, cfg)) => {
-                    *sig = new_sig;
-                    cb(&cfg);
+        for (name, name_ext, sig, cb) in self.cbs.iter_mut() {
+            let mut use_name = false;
+            //优先使用name_ext配置
+            if !name_ext.is_empty() {
+                let name = format!("{}/{}", name, name_ext);
+                match discovery.get_service::<String>(&name, sig).await {
+                    Ok(Config::Config(new_sig, cfg)) => {
+                        *sig = new_sig;
+                        cb(&cfg);
+                    }
+                    e @ Ok(Config::NotFound) => {
+                        use_name = true;
+                        log::warn!(
+                            "get service {} err {:?}, continue get whitout {}",
+                            name,
+                            e,
+                            name_ext
+                        );
+                    }
+                    //其余情况不能判定应使用name配置
+                    e => {
+                        log::warn!("get service {} err {:?}", name, e);
+                    }
                 }
-                _ => {}
+            }
+            if name_ext.is_empty() || use_name {
+                match discovery.get_service::<String>(name, sig).await {
+                    Ok(Config::Config(new_sig, cfg)) => {
+                        *sig = new_sig;
+                        cb(&cfg);
+                    }
+                    e => {
+                        log::warn!("get service {} err {:?}", name, e);
+                    }
+                }
             }
         }
     }
-    pub fn register(&mut self, path: String, cb: impl Fn(&str) + 'static + Send + Sync) {
-        self.cbs.push((path, "".to_string(), Box::new(cb)));
+    pub fn register(
+        &mut self,
+        path: String,
+        name_ext: &'static str,
+        cb: impl Fn(&str) + 'static + Send + Sync,
+    ) {
+        self.cbs
+            .push((path, name_ext, "".to_string(), Box::new(cb)));
     }
 }
 
