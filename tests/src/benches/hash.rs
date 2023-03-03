54adafc80ab4a4ba2dfc5e35f1a1506103022d0d
icycrystal4 <icycrystal4@gmail.com>
2023-03-03 13:22:13 +0800

    hash: 对比直接使用Crc32与通过enum使用的bench

--- tests/src/benches/hash.rs
+++ tests/src/benches/hash.rs
@@ -1,24 +1,22 @@
-use criterion::{black_box, Criterion};
+use criterion::Criterion;
 use ds::RingSlice;
-use sharding::hash::{Crc32, Hash};
+use sharding::hash::{Crc32, Hash, Hasher};
 pub(super) fn bench_crc32(c: &mut Criterion) {
-    let key = "ad_61fa9e8bc3624\r\nabcdjkfdajkd;aiejfjkd;ajflskdjfkkslajfiej";
+    let key = "ad_61fa24jfdkd\r\nabcdjkfdajkd;aiejfjkd;ajflskdjfkkslajfiej";
     let idx = key.find('\r').expect("return not foudn");
     let key: RingSlice = key.as_bytes().into();
     let mut group = c.benchmark_group("hash");
+    let crc32 = Hasher::Crc32(Crc32);
     group.bench_function("crc32_basic", |b| {
         b.iter(|| {
-            black_box({
-                // 模拟redis先找到key再计算hash
-                let idx = key.find_lf_cr(0).expect("no key found");
-                let raw_key = key.sub_slice(0, idx);
-                Crc32.hash(&raw_key)
-            });
+            crc32.hash(&key.slice(0, idx))
+            //black_box(crc32.hash(&key.slice(0, idx)));
         });
     });
     group.bench_function("crc32_simple", |b| {
         b.iter(|| {
-            black_box(Crc32.hash(&key.slice(0, idx)));
+            Crc32.hash(&key.slice(0, idx))
+            //black_box(Crc32.hash(&key.slice(0, idx)));
         });
     });
     group.finish();
