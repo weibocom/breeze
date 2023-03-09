42cf729e96dc0922c30e284f72f231b59b2715ac
kongkong <zhouthl@outlook.com>
2023-03-09 17:31:17 +0800

    build mysql cmd

--- tests/src/mysql_strategy.rs
+++ tests/src/mysql_strategy.rs
@@ -7,31 +7,30 @@ mod mysql_strategy {
     const SQL_UPDATE: &'static str = "update $db$.$tb$ set content=$v$ where id=$k$";
     const SQL_DELETE: &'static str = "delete from $db$.$tb$ where id=$k$";
     const SQL_SELECT: &'static str = "select content from $db$.$tb$ where id=$k$";
-    #[test]
-    fn test_get_sql() {
-        // let id = 3094373189550081i64;
-        let id = UuidSimulator::new().generate_id(1253239522000i64); // Tue Sep 18
+    // #[test]
+    // fn test_get_sql() {
+    //     // let id = 3094373189550081i64;
+    //     let id = UuidSimulator::new().generate_id(1253239522000i64); // Tue Sep 18
 
-        let mut sqls = HashMap::with_capacity(4);
-        sqls.insert("SQL_SELECT".to_string(), SQL_SELECT.to_string());
+    //     let mut sqls = HashMap::with_capacity(4);
+    //     sqls.insert("SQL_SELECT".to_string(), SQL_SELECT.to_string());
 
-        let s = Strategy::new(
-            "status".to_string(),
-            "status".to_string(),
-            "yymmdd".to_string(),
-            32,
-            1,
-            false,
-            sqls,
-            "crc32".to_string(),
-            "modula".to_string(),
-        );
-        let mut sql_cmd = s.build_sql("SQL_SELECT", id, id);
-        if sql_cmd != None {
-            println!("id: {}, sql: {}", id, sql_cmd.unwrap());
-        }
-    }
-
+    //     let s = Strategy::new(
+    //         "status".to_string(),
+    //         "status".to_string(),
+    //         "yymmdd".to_string(),
+    //         32,
+    //         1,
+    //         false,
+    //         sqls,
+    //         "crc32".to_string(),
+    //         "modula".to_string(),
+    //     );
+    //     let mut sql_cmd = s.build_sql("SQL_SELECT", id, id);
+    //     if sql_cmd != None {
+    //         println!("id: {}, sql: {}", id, sql_cmd.unwrap());
+    //     }
+    // }
     #[test]
     fn text_id_to_unix_time() {
         let id = 3379782484330149i64;
