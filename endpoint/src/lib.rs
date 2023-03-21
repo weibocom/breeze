9031969a374c02e60f9c44796410d100a9814d54
chenyong1 <chenyong1@MacBook-Pro-3.local>
2023-03-21 10:39:25 +0800

    调整默认下行超时时间为50ms

--- endpoint/src/lib.rs
+++ endpoint/src/lib.rs
@@ -16,9 +16,9 @@ pub use notify::Notify;
 // 不同资源默认的超时时间
 const TO_PHANTOM_M: Timeout = Timeout::from_millis(200);
 const TO_REDIS_M: Timeout = Timeout::from_millis(500);
-const TO_REDIS_S: Timeout = Timeout::from_millis(200);
+const TO_REDIS_S: Timeout = Timeout::from_millis(50);
 const TO_MC_M: Timeout = Timeout::from_millis(500);
-const TO_MC_S: Timeout = Timeout::from_millis(80);
+const TO_MC_S: Timeout = Timeout::from_millis(50);
 
 #[derive(Copy, Clone, Debug)]
 pub struct Timeout {
@@ -29,7 +29,7 @@ impl Timeout {
         Self { ms }
     }
     pub fn adjust(&mut self, ms: u32) {
-        self.ms = ms.max(100).min(6000) as u16;
+        self.ms = ms.max(50).min(6000) as u16;
     }
     pub fn to(mut self, ms: u32) -> Self {
         self.adjust(ms);
