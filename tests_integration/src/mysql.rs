ff85af64de5bd6d6a727830e156ef1796d38aca1
陈波 <chenbo@staff.weibo.com>
2023-03-24 11:43:04 +0800

    update Timezone for mysql table

--- tests_integration/src/mysql.rs
+++ tests_integration/src/mysql.rs
@@ -1,4 +1,7 @@
 use crate::mc_helper::*;
+use chrono::TimeZone;
+use chrono_tz::Asia::Shanghai;
+use endpoint::mysql::uuid::*;
 use memcache::MemcacheError;
 
 #[test]
@@ -7,4 +10,17 @@ fn get() {
     let key = "3094373189550081";
     let result: Result<Option<String>, MemcacheError> = client.get(key);
     assert_eq!(true, result.expect("ok").is_none());
+}
+
+#[test]
+fn time_testst() {
+    let id = 4839120888922294;
+    let mills = UuidHelper::get_time(id) * 1000;
+    let display = chrono::Utc
+        .timestamp_millis(mills)
+        .with_timezone(&Shanghai)
+        .format("%Y/%m/%d %H:%M")
+        .to_string();
+
+    println!("time:{}", display);
 }
