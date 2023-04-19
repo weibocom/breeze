dc2fba3b9ff67eefa79635e85dacceffbb56d1a9
icycrystal4 <icycrystal4@gmail.com>
2023-04-19 18:25:30 +0800

    request: 删除多余的接口定义

--- protocol/src/request.rs
+++ protocol/src/request.rs
@@ -1,9 +1,9 @@
-use crate::{callback::CallbackContext, Command, Context, Error, HashedCommand, Operation};
+use crate::{callback::CallbackContext, Command, Context, Error, HashedCommand};
+use sharding::BackendQuota;
 use std::{
     fmt::{self, Debug, Display, Formatter},
     ptr::NonNull,
 };
-use sharding::BackendQuota;
 
 pub struct Request {
     ctx: NonNull<CallbackContext>,
@@ -14,34 +14,34 @@ impl crate::Request for Request {
     fn start_at(&self) -> ds::time::Instant {
         self.ctx().start_at()
     }
-    #[inline]
-    fn len(&self) -> usize {
-        self.req().len()
-    }
-    #[inline]
-    fn cmd(&self) -> &HashedCommand {
-        self.req()
-    }
-    #[inline]
-    fn data(&self) -> &ds::RingSlice {
-        self.req().data()
-    }
+    //#[inline]
+    //fn len(&self) -> usize {
+    //    self.req().len()
+    //}
+    //#[inline]
+    //fn cmd(&self) -> &HashedCommand {
+    //    self.req()
+    //}
+    //#[inline]
+    //fn data(&self) -> &ds::RingSlice {
+    //    self.req().data()
+    //}
     //#[inline]
     //fn read(&self, oft: usize) -> &[u8] {
     //    self.req().read(oft)
     //}
-    #[inline]
-    fn operation(&self) -> Operation {
-        self.req().operation()
-    }
-    #[inline]
-    fn hash(&self) -> i64 {
-        self.req().hash()
-    }
-    #[inline]
-    fn sentonly(&self) -> bool {
-        self.req().sentonly()
-    }
+    //#[inline]
+    //fn operation(&self) -> Operation {
+    //    self.req().operation()
+    //}
+    //#[inline]
+    //fn hash(&self) -> i64 {
+    //    self.req().hash()
+    //}
+    //#[inline]
+    //fn sentonly(&self) -> bool {
+    //    self.req().sentonly()
+    //}
     #[inline]
     fn on_noforward(&mut self) {
         self.ctx().on_noforward();
@@ -93,10 +93,10 @@ impl Request {
         Self { ctx }
     }
 
-    #[inline]
-    fn req(&self) -> &HashedCommand {
-        self.ctx().request()
-    }
+    //#[inline]
+    //fn req(&self) -> &HashedCommand {
+    //    self.ctx().request()
+    //}
 
     // #[inline]
     // fn req_mut(&self) -> &mut HashedCommand {
