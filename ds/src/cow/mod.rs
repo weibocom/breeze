1027c00298f8f4d55cb0c3662006dcd3c9c4719c
kongkong <zhouthl@outlook.com>
2023-05-10 14:56:25 +0800

    cow中持有arc<t>, read得到Arc的克隆

--- ds/src/cow/mod.rs
+++ ds/src/cow/mod.rs
@@ -4,7 +4,7 @@ pub use read::*;
 mod write;
 pub use write::*;
 
-pub fn cow<T: Clone>(t: T) -> (CowWriteHandle<T>, CowReadHandle<T>) {
+pub fn cow<T: Clone + Send + Sync>(t: T) -> (CowWriteHandle<T>, CowReadHandle<T>) {
     let rx: CowReadHandle<T> = t.into();
     let tx = CowWriteHandle::from(rx.clone());
     (tx, rx)
--- ds/src/cow/write.rs
+++ ds/src/cow/write.rs
@@ -1,15 +1,18 @@
 use super::CowReadHandle;
-use std::sync::atomic::Ordering::{AcqRel, Acquire};
+use std::sync::{
+    atomic::Ordering::{AcqRel, Acquire},
+    Arc,
+};
 pub struct CowWriteHandle<T> {
     r_handle: CowReadHandle<T>,
 }
-impl<T: Clone> CowWriteHandle<T> {
+impl<T: Clone + Send + Sync> CowWriteHandle<T> {
     pub(crate) fn from(r_handle: CowReadHandle<T>) -> Self {
         Self { r_handle }
     }
     // 如果上一次write请求还未结束，则进入spin状态.
-    pub fn write<F: FnMut(&mut T)>(&mut self, mut f: F) {
-        let mut t: T = self.r_handle.read(|t| t.clone());
+    pub fn write<F: FnOnce(&mut T)>(&mut self, f: F) {
+        let mut t: T = self.r_handle.copy();
         f(&mut t);
         self.update(t);
     }
@@ -21,7 +24,7 @@ impl<T: Clone> CowWriteHandle<T> {
             .compare_exchange(false, true, AcqRel, Acquire)
             .expect("lock failed");
 
-        let guard = self.get();
+        // let guard = self.get();
         // 在r_handle.update中更新epoch为false.
         self.r_handle.update(t);
 
@@ -30,14 +33,14 @@ impl<T: Clone> CowWriteHandle<T> {
             .compare_exchange(true, false, AcqRel, Acquire)
             .expect("unlock failed");
 
-        drop(guard);
+        // drop(guard);
     }
     #[inline]
     pub fn copy(&self) -> T {
-        self.r_handle.read(|t| t.clone())
+        self.r_handle.copy()
     }
     #[inline]
-    pub fn get(&self) -> super::ReadGuard<'_, T> {
+    pub fn get(&self) -> Arc<T> {
         self.r_handle.get()
     }
 }
