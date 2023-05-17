0cbb2bad4c82c82866ac1de5ea4630eac19eb057
kongkong <zhouthl@outlook.com>
2023-05-17 11:39:21 +0800

    top refresh test

--- tests_integration/src/redis_helper.rs
+++ tests_integration/src/redis_helper.rs
@@ -4,5 +4,5 @@ pub fn get_conn(host: &str) -> redis::Connection {
     let client = Client::open(hoststr).expect("get client err");
     client
         .get_connection()
-        .expect(&("get conn err".to_string() + host))
+        .expect(&("get conn err ".to_string() + host))
 }
