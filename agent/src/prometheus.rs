04cb0b9425dd7c852b72ca28082658d5f95b2318
kongkong <zhouthl@outlook.com>
2023-05-19 17:34:37 +0800

    替换metric的http框架未hyper，减少非console 特性版本二进制大小

--- agent/src/prometheus.rs
+++ agent/src/prometheus.rs
@@ -1,45 +1,33 @@
 use metrics::prometheus::Prometheus;
-use rocket::{
-    http::Status,
-    request::Request,
-    response::{stream::ReaderStream, Responder, Response},
-    Build, Rocket,
-};
-
-pub(crate) fn init_routes(rocket: Rocket<Build>) -> Rocket<Build> {
-    rocket.mount("/metrics", routes![prometheus_metrics])
-}
-
-#[get("/")]
-fn prometheus_metrics() -> PrometheusMetricsResponse {
-    PrometheusMetricsResponse {}
-}
-
-pub struct PrometheusMetricsResponse {}
 
 use ds::lock::Lock;
-use lazy_static::lazy_static;
 use ds::time::Instant;
+use hyper::{Body, Response, StatusCode};
+use lazy_static::lazy_static;
+use tokio_util::io::ReaderStream;
 
 lazy_static! {
     static ref LAST: Lock<Instant> = Instant::now().into();
 }
 
-impl<'r> Responder<'r, 'r> for PrometheusMetricsResponse {
-    fn respond_to(self, _: &Request) -> rocket::response::Result<'r> {
-        let mut response = Response::build();
-        if let Ok(mut last) = LAST.try_lock() {
-            let secs = last.elapsed().as_secs_f64();
-            if secs >= 8f64 {
-                *last = Instant::now();
-                let metrics = Prometheus::new(secs);
-                let stream: ReaderStream<Prometheus> = metrics.into();
-                return response.streamed_body(stream).ok();
-            }
-            return response.status(Status::NotModified).ok();
+pub async fn prometheus_metrics() -> Result<Response<Body>, hyper::Error> {
+    let rsp = Response::builder();
+    if let Ok(mut last) = LAST.try_lock() {
+        let secs = last.elapsed().as_secs_f64();
+        if secs >= 8f64 {
+            *last = Instant::now();
+            let metrics = ReaderStream::new(Prometheus::new(secs));
+            return Ok(rsp.body(Body::wrap_stream(metrics)).unwrap());
         }
-        response.status(Status::Processing).ok()
+        return Ok(rsp
+            .status(StatusCode::NOT_MODIFIED)
+            .body(Body::default())
+            .unwrap());
     }
+    Ok(rsp
+        .status(StatusCode::PROCESSING)
+        .body(Body::default())
+        .unwrap())
 }
 
 // 定期发心跳
