bb1a5af73b11e1b122f2f37974a172f6b5b90522
xiaowang <xiaowang@staff.weibo.com>
2023-05-25 19:22:26 +0800

    rustfmt

--- ds/src/decrypt.rs
+++ ds/src/decrypt.rs
@@ -1,9 +1,12 @@
-use openssl::pkey::{PKey};
+use openssl::pkey::PKey;
 use openssl::rsa::Padding;
 
 // key_pem: rsa私钥,pem格式
 // encrypted_data: 加密数据
-pub fn decrypt_password(key_pem: &String, encrypted_data: &Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub fn decrypt_password(
+    key_pem: &String,
+    encrypted_data: &Vec<u8>,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let private_key = PKey::private_key_from_pem(key_pem.as_bytes())?;
     let rsa = private_key.rsa()?;
     // 缓冲区大小必须 >= 密钥长度
