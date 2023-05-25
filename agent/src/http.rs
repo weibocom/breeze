c0ba887dbc481a52ae390c3b04f255adbf1bceec
kongkong <zhouthl@outlook.com>
2023-05-25 11:16:48 +0800

    替换metric的http框架未hyper，减少非console 特性版本二进制大小 (#309)
    * 替换metric的http框架未hyper，减少非console 特性版本二进制大小
    * 集成测试暂不测试console
    * 增加开启console的提示
    * rsponse 方式改为官方用例的方式

--- agent/src/http.rs
+++ agent/src/http.rs
@@ -1,27 +1,29 @@
-use rocket::{
-    config::{Config, Sig},
-    log::LogLevel,
-};
-use std::net::Ipv4Addr;
-// 必须运行在tokio的runtime环境中
-pub(super) fn start_http_server(ctx: &context::Context) {
-    if cfg!(feature = "http") {
-        log::info!("starting http server!");
-        let mut c = Config::default();
-        c.shutdown.ctrlc = false;
-        c.shutdown.signals.insert(Sig::Hup);
-        c.address = Ipv4Addr::new(0, 0, 0, 0).into();
-        c.port = ctx.port;
-        c.log_level = LogLevel::Critical;
-        c.workers = 4;
-        let mut rocket = rocket::custom(c);
-        rocket = crate::console::init_routes(rocket, ctx);
-        rocket = crate::prometheus::init_routes(rocket);
-        //rocket = rocket.attach(rocket_async_compression::Compression::fairing());
-        rt::spawn(async {
-            if let Err(_e) = rocket.launch().await {
-                log::error!("launch rocket failed: {:?}", _e);
-            };
-        });
+#![cfg(feature = "http")]
+
+use super::prometheus::prometheus_metrics;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use rt::spawn;
+
+pub(crate) fn start(ctx: &context::Context) {
+    let addr = ([0, 0, 0, 0], ctx.port).into();
+
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(route)) });
+    let server = Server::bind(&addr).serve(service);
+    spawn(async {
+        if let Err(_e) = server.await {
+            log::error!("hyper failed: {:?}", _e);
+        };
+    });
+}
+
+async fn route(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/metrics") => prometheus_metrics().await,
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
     }
 }
