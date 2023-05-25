cf38de291c050fc91e858735fef5d26019d1f1af
kongkong <zhouthl@outlook.com>
2023-05-25 11:16:48 +0800

    替换metric的http框架未hyper，减少非console 特性版本二进制大小 (#309)
    * 替换metric的http框架未hyper，减少非console 特性版本二进制大小
    * 集成测试暂不测试console
    * 增加开启console的提示
    * rsponse 方式改为官方用例的方式

--- agent/src/console.rs
+++ agent/src/console.rs
@@ -1,13 +1,30 @@
-use context::Context;
-use rocket::{Build, Rocket};
-
+// 必须运行在tokio的runtime环境中
 #[cfg(feature = "console-api")]
-pub(crate) fn init_routes(rocket: Rocket<Build>, ctx: &Context) -> Rocket<Build> {
+pub(super) fn start_console(ctx: &context::Context) {
+    log::error!("you should start console-api, this feature not supported now");
+    return;
+    use rocket::{
+        config::{Config, Sig},
+        log::LogLevel,
+    };
+    use std::net::Ipv4Addr;
+    log::info!("starting console http server!");
+    let mut c = Config::default();
+    c.shutdown.ctrlc = false;
+    c.shutdown.signals.insert(Sig::Hup);
+    c.address = Ipv4Addr::new(0, 0, 0, 0).into();
+    c.port = ctx.port;
+    c.log_level = LogLevel::Critical;
+    c.workers = 4;
+    let rocket = rocket::custom(c);
+
     rt::spawn(api::start_whitelist_refresh(ctx.whitelist_host.clone()));
     let rocket = api::routes(rocket);
-    rocket
-}
-#[cfg(not(feature = "console-api"))]
-pub(crate) fn init_routes(rocket: Rocket<Build>, _ctx: &Context) -> Rocket<Build> {
-    rocket
+
+    //rocket = rocket.attach(rocket_async_compression::Compression::fairing());
+    rt::spawn(async {
+        if let Err(_e) = rocket.launch().await {
+            log::error!("launch rocket failed: {:?}", _e);
+        };
+    });
 }
