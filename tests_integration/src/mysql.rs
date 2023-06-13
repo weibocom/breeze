ebe03b0463ae226e49f79db00545caecb09ff83b
kongkong <zhouthl@outlook.com>
2023-06-13 17:47:49 +0800

    escape content

--- tests_integration/src/mysql.rs
+++ tests_integration/src/mysql.rs
@@ -17,11 +17,12 @@ fn get() {
 #[test]
 fn set() {
     let client = mc_get_conn("mysql");
-    let key = "4892225613598475";
-    client.add(key, "3", 10000).unwrap();
+    let key = "3503157055392381";
+    let val = r#"{"created_at":"","id":3503157055392381,"text":"","favorited":false,"truncated":false,"in_reply_to_status_id":"","in_reply_to_user_id":"","in_reply_to_screen_name":"","pic_ids":[],"geo":null,"mid":null,"is_show_bulletin":0,"state":0,"api_state":3,"flag":0,"list_id":0,"weiboState":0,"ip":null}"#;
+    client.add(key, val, 10000).unwrap();
     let result: Result<Option<String>, MemcacheError> = client.get(key);
     println!("{:?}", result);
-    assert_eq!("3", result.unwrap().unwrap());
+    assert_eq!(val, result.unwrap().unwrap());
 }
 
 // #[test]
