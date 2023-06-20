f0c3eca41164245547cc1c3a09f23b01025490ed
陈波 <chenbo@staff.weibo.com>
2023-06-20 09:16:48 +0800

    mpsc的tx在从channel读出数据后不需要减1

--- ds/src/chan/mpsc.rs
+++ ds/src/chan/mpsc.rs
@@ -48,8 +48,6 @@ impl<T> Receiver<T> {
         self.inner.poll_recv(cx).map(|t| {
             t.map(|v| {
                 self.rx += 1;
-                // TODO 收到请求后，tx需要减1?
-                self.tx.fetch_sub(1, AcqRel);
                 v
             })
         })
