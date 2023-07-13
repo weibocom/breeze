93a2c34fcccdcecf71942290d20f85abf2a7c11d
陈波 <chenbo@staff.weibo.com>
2023-07-13 10:56:02 +0800

    add missing files

---
+++ protocol/src/kv/packet.rs
@@ -0,0 +1,83 @@
+use std::fmt::Display;
+
+use byteorder::{ByteOrder, LittleEndian};
+use ds::RingSlice;
+
+use crate::{Error, Result};
+
+const HEADER_LEN: usize = 4;
+
+/// mysql的响应或请求packet的原始数据封装，packet data可能包含多个packet；
+/// 如Com_Query的响应中包含一个定义columns的meta packet 和 若干个row packet。
+#[derive(Debug)]
+pub(super) struct PacketData {
+    inner: RingSlice,
+}
+
+impl From<RingSlice> for PacketData {
+    #[inline(always)]
+    fn from(s: RingSlice) -> Self {
+        Self { inner: s }
+    }
+}
+
+/// 基于RingSlice解析mysql协议
+impl PacketData {
+    /// 解析packet header，
+    pub(super) fn parse_header(&self, oft: &mut usize) -> Result<PacketHeader> {
+        // mysql packet至少需要4个字节来读取sequence id
+        if self.left_len(*oft) <= HEADER_LEN {
+            return Err(Error::ProtocolIncomplete);
+        }
+
+        // let payload_len = LittleEndian::read_u24(header_bytes) as usize;
+        // self.payload_len = raw_chunk_len;
+        // let seq = header_bytes[3];
+
+        // TODO RingSlice 解析的标准用法，原则：最大限度减少copy，能否进一步优化？ fishermen
+        let (payload_len, seq) = match self.inner.try_oneway_slice(*oft, HEADER_LEN) {
+            Some(data) => (LittleEndian::read_u24(data) as usize, data[3]),
+            None => {
+                let bytes = self.inner.dump_ring_part(*oft, HEADER_LEN);
+                (LittleEndian::read_u24(&bytes) as usize, bytes[3])
+            }
+        };
+
+        *oft = *oft + HEADER_LEN;
+        log::debug!("+++ payload_len: {}, seq: {}", payload_len, seq);
+
+        Ok(PacketHeader { payload_len, seq })
+    }
+
+    #[inline(always)]
+    pub(super) fn left_len(&self, oft: usize) -> usize {
+        self.inner.len() - oft
+    }
+}
+
+impl std::ops::Deref for PacketData {
+    type Target = RingSlice;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl Display for PacketData {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "mysql:[{:?}]", self.inner)
+    }
+}
+
+pub(super) struct PacketHeader {
+    pub(super) payload_len: usize,
+    pub(super) seq: u8,
+}
+
+impl PacketHeader {
+    // #[inline(always)]
+    // pub(super) fn packet_len(&self) -> usize {
+    //     self.body_len + HEADER_LEN
+    // }
+}
