b62300b7fb0780ef1611f873df3baba46322b91a
kongkong <zhouthl@outlook.com>
2023-11-01 18:35:58 +0800

    记录后端连接qps

--- stream/src/checker.rs
+++ stream/src/checker.rs
@@ -56,6 +56,7 @@ impl<P, Req> BackendChecker<P, Req> {
         Req: Request,
     {
         let path_addr = self.path.clone().push(&self.addr);
+        let mut all_conns = path_addr.qps("conn");
         let mut m_timeout = path_addr.qps("timeout");
         let mut auth_failed = path_addr.status("auth_failed");
         let mut unexpected_resp = path_addr.num("unexpected_resp");
@@ -63,6 +64,7 @@ impl<P, Req> BackendChecker<P, Req> {
         let mut reconn = crate::reconn::ReconnPolicy::new(&path_addr);
         metrics::incr_task();
         while !self.finish.get() {
+            all_conns += 1;
             let stream = self.reconnect().await;
             if stream.is_none() {
                 // 连接失败，按策略sleep
