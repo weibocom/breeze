9bca2bbc9731c93a9fb22b18f4b53e9e271b5c6c
icycrystal4 <icycrystal4@gmail.com>
2023-11-24 15:25:55 +0800

    ring_slice: 提供统一的基于range的数据访问策略

--- ds/src/lib.rs
+++ ds/src/lib.rs
@@ -26,12 +26,49 @@ pub use asserts::*;
 mod bits;
 pub use bits::*;
 
+use std::ptr::copy_nonoverlapping as copy;
+
 pub trait BufWriter {
     fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()>;
     #[inline]
     fn write_seg_all(&mut self, buf0: &[u8], buf1: &[u8]) -> std::io::Result<()> {
         self.write_all(buf0)?;
         self.write_all(buf1)
+    }
+}
+
+impl BufWriter for Vec<u8> {
+    #[inline]
+    fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()> {
+        self.reserve(buf.len());
+        let len = self.len();
+        (&mut self[len..]).write_all(buf)?;
+        unsafe { self.set_len(len + buf.len()) };
+        Ok(())
+    }
+    #[inline]
+    fn write_seg_all(&mut self, buf0: &[u8], buf1: &[u8]) -> std::io::Result<()> {
+        let reserve_len = buf0.len() + buf1.len();
+        self.reserve(reserve_len);
+        let len = self.len();
+        (&mut self[len..]).write_seg_all(buf0, buf1)?;
+        unsafe { self.set_len(len + reserve_len) };
+        Ok(())
+    }
+}
+impl BufWriter for [u8] {
+    #[inline(always)]
+    fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()> {
+        debug_assert!(self.len() >= buf.len());
+        unsafe { copy(buf.as_ptr(), self.as_mut_ptr(), buf.len()) };
+        Ok(())
+    }
+    #[inline(always)]
+    fn write_seg_all(&mut self, buf0: &[u8], buf1: &[u8]) -> std::io::Result<()> {
+        debug_assert!(self.len() >= buf0.len() + buf1.len());
+        unsafe { copy(buf0.as_ptr(), self.as_mut_ptr(), buf0.len()) };
+        unsafe { copy(buf1.as_ptr(), self.as_mut_ptr().add(buf0.len()), buf1.len()) };
+        Ok(())
     }
 }
 
