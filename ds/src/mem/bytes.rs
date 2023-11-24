a8ac9c0bbbc0bc7226b45ff8bd30316d9d7d45aa
icycrystal4 <icycrystal4@gmail.com>
2023-11-24 11:31:26 +0800

    ring_slice: 提供统一的基于range的数据访问策略

--- ds/src/mem/bytes.rs
+++ ds/src/mem/bytes.rs
@@ -27,3 +27,44 @@ pub trait ByteOrder {
     fn f32_le(&self, oft: usize) -> f32;
     fn f64_le(&self, oft: usize) -> f64;
 }
+
+pub trait Range {
+    fn range(&self, slice: &RingSlice) -> (usize, usize);
+}
+
+type Offset = usize;
+impl Range for Offset {
+    fn range(&self, slice: &RingSlice) -> (usize, usize) {
+        debug_assert!(*self <= slice.len());
+        (*self, slice.len())
+    }
+}
+
+impl Range for std::ops::Range<usize> {
+    #[inline(always)]
+    fn range(&self, slice: &RingSlice) -> (usize, usize) {
+        debug_assert!(self.start <= slice.len());
+        debug_assert!(self.end <= slice.len());
+        (self.start, self.end)
+    }
+}
+impl Range for std::ops::RangeFrom<usize> {
+    #[inline(always)]
+    fn range(&self, slice: &RingSlice) -> (usize, usize) {
+        debug_assert!(self.start <= slice.len());
+        (self.start, slice.len())
+    }
+}
+impl Range for std::ops::RangeTo<usize> {
+    #[inline(always)]
+    fn range(&self, slice: &RingSlice) -> (usize, usize) {
+        debug_assert!(self.end <= slice.len());
+        (0, self.end)
+    }
+}
+impl Range for std::ops::RangeFull {
+    #[inline(always)]
+    fn range(&self, slice: &RingSlice) -> (usize, usize) {
+        (0, slice.len())
+    }
+}
