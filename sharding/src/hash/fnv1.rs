c3a391e03e2b30fbd631d6e4de6574afbbf99f30
陈波 <chenbo@staff.weibo.com>
2024-01-30 17:29:25 +0800

    support fnv1a_64 以及 twemproxy支持的原始版本的ketama

---
+++ sharding/src/hash/fnv1.rs
@@ -0,0 +1,18 @@
+/// 按需支持fnv1系列所有相关的hash算法，目前支持fnv1a_64
+
+#[derive(Debug, Default, Clone)]
+pub struct Fnv1a64;
+
+const FNV_64_INIT: u64 = 0xcbf29ce484222325;
+const FNV_64_PRIME: u64 = 0x100000001b3;
+
+impl super::Hash for Fnv1a64 {
+    fn hash<S: crate::HashKey>(&self, key: &S) -> i64 {
+        let mut hash = FNV_64_INIT as u32;
+        for i in 0..key.len() {
+            hash ^= key.at(i) as u32;
+            hash = hash.wrapping_mul(FNV_64_PRIME as u32);
+        }
+        hash as i64
+    }
+}
