e5a12bded7f020a13d1eb68ea2678b8c0b6f973b
icycrystal4 <icycrystal4@gmail.com>
2024-03-08 18:43:31 +0800

    Writer: 统一writer

--- ds/src/vec/mod.rs
+++ ds/src/vec/mod.rs
@@ -1,49 +1,6 @@
 mod ephemera;
 pub use ephemera::*;
 
-pub trait Buffer: Sized {
-    fn write<D: AsRef<[u8]>>(&mut self, data: D);
-    #[inline(always)]
-    fn write_u16(&mut self, num: u16) {
-        self.write(num.to_be_bytes());
-    }
-    #[inline(always)]
-    fn write_u32(&mut self, num: u32) {
-        self.write(num.to_be_bytes());
-    }
-    #[inline(always)]
-    fn write_u64(&mut self, num: u64) {
-        self.write(num.to_be_bytes());
-    }
-    #[inline(always)]
-    fn write_slice(&mut self, slice: &crate::RingSlice) {
-        use crate::Slicer;
-        slice.with_seg(0, |data, _oft, _seg| {
-            self.write(data);
-        });
-    }
-}
-impl Buffer for Vec<u8> {
-    #[inline]
-    fn write<D: AsRef<[u8]>>(&mut self, data: D) {
-        let b = data.as_ref();
-        use std::ptr::copy_nonoverlapping as copy;
-        self.reserve(b.len());
-        unsafe {
-            copy(
-                b.as_ptr() as *const u8,
-                self.as_mut_ptr().add(self.len()),
-                b.len(),
-            );
-            self.set_len(self.len() + b.len());
-        }
-    }
-    #[inline(always)]
-    fn write_slice(&mut self, slice: &crate::RingSlice) {
-        slice.copy_to_vec(self);
-    }
-}
-
 pub trait Add<T> {
     fn add(&mut self, t: T);
 }
