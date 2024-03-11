aa41764f9a7e7df0221e74d7480b75750a893ea4
icycrystal4 <icycrystal4@gmail.com>
2024-03-11 15:36:45 +0800

    dns: 约有1.8k/s的对象分配是由dns resolver提供，先将lookup单独抽离到独立的module。后续优化

--- discovery/src/dns/lookup.rs
+++ discovery/src/dns/lookup.rs
@@ -1,22 +1,22 @@
 use super::Record;
 pub(super) struct Lookup {
-    resolver: TokioAsyncResolver,
+    //resolver: TokioAsyncResolver,
 }
 
-use trust_dns_resolver::{lookup_ip::LookupIp, TokioAsyncResolver};
+//use trust_dns_resolver::{lookup_ip::LookupIp, TokioAsyncResolver};
 
 use std::net::IpAddr;
 impl Lookup {
     pub(super) fn new() -> Self {
         Self {
-            resolver: TokioAsyncResolver::tokio_from_system_conf()
-                .expect("Failed to create the resolver"),
+            //resolver: TokioAsyncResolver::tokio_from_system_conf()
+            //    .expect("Failed to create the resolver"),
         }
     }
-    async fn lookup(&self, host: &str) -> std::io::Result<IpAddrLookup> {
-        let ips = self.resolver.lookup_ip(host).await?;
-        Ok(IpAddrLookup::LookupIp(ips))
-    }
+    //async fn lookup(&self, host: &str) -> std::io::Result<IpAddrLookup> {
+    //    let ips = self.resolver.lookup_ip(host).await?;
+    //    Ok(IpAddrLookup::LookupIp(ips))
+    //}
     fn dns_lookup(&self, host: &str) -> std::io::Result<IpAddrLookup> {
         let ips = dns_lookup::lookup_host(host)?;
         Ok(IpAddrLookup::AddrInfoIter(ips))
@@ -36,10 +36,7 @@ impl Lookup {
             let ips = ret.unwrap();
             if r.refresh(host, ips) {
                 num += 1;
-                if num == 1 {
-                    assert!(cache.is_none());
-                    cache = Some(host.to_string());
-                }
+                (num == 1).then(|| cache = Some(host.to_string()));
             }
         }
         (num, cache)
@@ -47,17 +44,17 @@ impl Lookup {
 }
 
 pub(super) enum IpAddrLookup {
-    LookupIp(LookupIp),
+    //LookupIp(LookupIp),
     AddrInfoIter(Vec<IpAddr>),
 }
 impl IpAddrLookup {
     pub(super) fn visit_v4(&self, mut v: impl FnMut(std::net::Ipv4Addr)) {
         match self {
-            Self::LookupIp(ips) => ips.iter().for_each(|ip| {
-                if let IpAddr::V4(ip) = ip {
-                    v(ip);
-                }
-            }),
+            //Self::LookupIp(ips) => ips.iter().for_each(|ip| {
+            //    if let IpAddr::V4(ip) = ip {
+            //        v(ip);
+            //    }
+            //}),
             Self::AddrInfoIter(iter) => {
                 for ip in iter.iter() {
                     if let std::net::IpAddr::V4(ip) = ip {
