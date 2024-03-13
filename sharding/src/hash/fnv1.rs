7518b19409032bbaa2947aa94bba1ca527323225
陈波 <chenbo@staff.weibo.com>
2024-03-13 12:05:47 +0800

    hash算法中的下划线不能用在结构名，统一改为F

--- sharding/src/hash/fnv1.rs
+++ sharding/src/hash/fnv1.rs
@@ -1,12 +1,12 @@
 /// 按需支持fnv1系列所有相关的hash算法，目前支持fnv1a_64
 
 #[derive(Debug, Default, Clone)]
-pub struct Fnv1a_64;
+pub struct Fnv1aF64;
 
 const FNV_64_INIT: u64 = 0xcbf29ce484222325;
 const FNV_64_PRIME: u64 = 0x100000001b3;
 
-impl super::Hash for Fnv1a_64 {
+impl super::Hash for Fnv1aF64 {
     fn hash<S: crate::HashKey>(&self, key: &S) -> i64 {
         let mut hash = FNV_64_INIT as u32;
         for i in 0..key.len() {
