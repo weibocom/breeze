e0161c86621e096539563557b039be38216c87f2
icycrystal4 <icycrystal4@gmail.com>
2024-03-22 12:08:11 +0800

    top discovery: 简化处理逻辑。

--- discovery/src/update.rs
+++ discovery/src/update.rs
@@ -1,28 +1,24 @@
 // 定期更新discovery.
-use super::{Discover, ServiceId, TopologyWrite};
+use super::{Discover, TopologyWrite};
 use ds::chan::Receiver;
 use ds::time::{interval, Duration};
 
-use crate::cache::DiscoveryCache;
-use crate::path::ToName;
 use std::collections::HashMap;
-use std::sync::atomic::{AtomicUsize, Ordering::*};
 
 pub async fn watch_discovery<D, T>(
     snapshot: String,
     discovery: D,
-    rx: Receiver<T>,
+    rx: Receiver<(String, T)>,
     tick: Duration,
     cb: super::fixed::Fixed,
 ) where
-    T: Send + TopologyWrite + ServiceId + 'static + Sync,
+    T: Send + TopologyWrite + 'static + Sync,
     D: Send + Sync + Discover + Unpin + 'static,
 {
     let tick = Duration::from_secs(3).max(tick);
-    let cache = DiscoveryCache::new(discovery);
     let mut refresher = Refresher {
         snapshot,
-        discovery: cache,
+        discovery,
         rx,
         tick,
         cb,
@@ -31,17 +27,17 @@ pub async fn watch_discovery<D, T>(
 }
 
 struct Refresher<D, T> {
-    discovery: DiscoveryCache<D>,
+    discovery: D,
     snapshot: String,
     tick: Duration,
-    rx: Receiver<T>,
+    rx: Receiver<(String, T)>,
     cb: super::fixed::Fixed,
 }
 
 impl<D, T> Refresher<D, T>
 where
     D: Discover + Send + Unpin + Sync,
-    T: Send + TopologyWrite + ServiceId + 'static + Sync,
+    T: Send + TopologyWrite + 'static + Sync,
 {
     async fn watch(&mut self) {
         log::info!("task started ==> topology refresher");
@@ -49,51 +45,258 @@ where
         let period = Duration::from_secs(1);
         let cycle = (self.tick.as_secs_f64() / period.as_secs_f64()).ceil() as usize;
         let mut cycle_i = 0usize;
-        let mut first_cycle = true;
         let mut tick = interval(period);
-        self.cb.with_discovery(self.discovery.inner()).await;
-        let mut services = HashMap::new();
-        // 每秒钟处理一次，一次只处理部分service。
+        self.cb.with_discovery(&self.discovery).await;
+        let mut services = Services::new(&self.snapshot);
         // 每个周期结束清理缓存
         loop {
-            while let Ok(t) = self.rx.try_recv() {
-                let service = t.service().name();
-                if services.contains_key(&service) {
-                    log::error!("service duplicatedly registered:{}", service);
-                } else {
-                    static SEQ: AtomicUsize = AtomicUsize::new(0);
-                    let id = SEQ.fetch_add(1, Relaxed);
-                    log::debug!("service path:{:?} registered ", service);
-                    let mut t: crate::cfg::Config<T> = t.into();
-                    t.init(&self.snapshot, &mut self.discovery).await;
-                    services.insert(service, (id, t));
-                }
-            }
-            for (_name, (id, service)) in services.iter_mut() {
-                // 每秒钟只更新部分。
-                if cycle_i == *id % cycle {
-                    service
-                        .check_update(&self.snapshot, &mut self.discovery)
-                        .await;
-                }
-            }
-
-            for (_service, (_id, t)) in services.iter_mut() {
-                t.try_load();
+            while let Ok((name, t)) = self.rx.try_recv() {
+                log::info!("receive new service: {}", name);
+                services.register(name, t, &self.discovery).await;
             }
             cycle_i += 1;
-            if cycle_i == cycle {
-                self.cb.with_discovery(self.discovery.inner()).await;
-                cycle_i = 0;
-                // 清空缓存
-                self.discovery.clear();
+
+            // 部分服务更新配置后，需要重新加载
+            if cycle_i <= cycle || cycle_i % cycle == 0 {
+                services.check_load().await;
+                if !self.cb.inited() || cycle_i % cycle == 0 {
+                    self.cb.with_discovery(&self.discovery).await;
+                }
             }
-            // 第一个循环处于冷启动期，需要加快固定任务的加载频率，避免请求失败导致其他问题(如socks)
-            if first_cycle && !self.cb.inited() {
-                self.cb.with_discovery(self.discovery.inner()).await;
-                first_cycle = false;
+            let i = cycle_i % cycle;
+            // 更新部分
+            for (idx, group) in services.groups.iter_mut().enumerate() {
+                if idx % cycle == i {
+                    group.refresh(&self.snapshot, &self.discovery).await;
+                } else {
+                    group.refresh_new(&self.snapshot, &self.discovery).await;
+                }
             }
+            // 更新新注册的
             tick.tick().await;
+        }
+    }
+}
+
+struct Services<T> {
+    snapshot: String,
+    indices: HashMap<String, usize>,
+    groups: Vec<ServiceGroup<T>>,
+}
+impl<T: TopologyWrite> Services<T> {
+    fn new(snapshot: &str) -> Self {
+        Self {
+            snapshot: snapshot.to_string(),
+            groups: Vec::with_capacity(64),
+            indices: HashMap::with_capacity(64),
+        }
+    }
+    async fn check_load(&mut self) {
+        for g in self.groups.iter_mut() {
+            g.load();
+        }
+    }
+    fn get_group(&mut self, group: &str) -> Option<&mut ServiceGroup<T>> {
+        let &idx = self.indices.get(group)?;
+        assert!(idx < self.groups.len());
+        Some(&mut self.groups[idx])
+    }
+    // name的格式。 dir0+dir1+dir2+...+group:namespace
+    // 规范：最后一个+号之后的是group:namespace
+    // namespace是可选。
+    // namespace之前的是group的路径，如：分隔符为'+'。
+    async fn register<D: Discover>(&mut self, name: String, top: T, d: &D) {
+        let group = name.split('+').last().expect("name");
+        let mut group_namespace = group.split(':');
+        let group = group_namespace.next().expect("group");
+        let (path, service) = match group_namespace.next() {
+            Some(ns) => (&name[..name.len() - ns.len() - 1], ns),
+            None => (name.as_str(), group),
+        };
+
+        let g = match self.get_group(group) {
+            Some(g) => g,
+            None => {
+                let idx = self.groups.len();
+                let local = path.to_string();
+                let remote = path.replace('+', "/");
+                self.groups
+                    .push(ServiceGroup::new(group.to_string(), local, remote));
+                self.indices.insert(group.to_string(), idx);
+                let g = self.groups.get_mut(idx).expect("not push");
+                assert_eq!(g.name, group);
+                g.init(&self.snapshot, d).await;
+                g
+            }
+        };
+        log::info!("register service: {} => {}", name, group);
+        g.register(service.to_string(), top);
+    }
+}
+
+// 一个group会有一个或者多个namespace,共享一个group配置。
+struct ServiceGroup<T> {
+    changed: bool,
+    name: String, // group的名称。
+    local_path: String,
+    path: String, // 从discover获取配置时使用group的path。
+    sig: String,
+    cfg: String,
+    cache: HashMap<String, String>,
+    namespaces: Vec<Service<T>>,
+}
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+impl<T: TopologyWrite> ServiceGroup<T> {
+    // 先从snapshot获取配置。然后再从discover刷新。
+    async fn init<D: Discover>(&mut self, snapshot: &str, d: &D) {
+        self.load_from_snapshot(snapshot).await;
+        self.load_from_discover(snapshot, d).await;
+    }
+    fn load(&mut self) {
+        for s in &mut self.namespaces {
+            if s.top.need_load() {
+                s.top.load();
+            }
+        }
+    }
+    // 第一行是sig
+    // 第二行是group name
+    // 后面是cfg
+    async fn load_from_snapshot(&mut self, snapshot: &str) -> Option<()> {
+        let path = format!("{}/{}", snapshot, self.local_path);
+        // 返回第一行与剩余的内容
+        fn take_line(mut s: String) -> Option<(String, String)> {
+            let idx = s.find("\n")?;
+            let left = s.split_off(idx + 1);
+            s.pop();
+            if s.len() > 0 && s.as_bytes()[s.len() - 1] == b'\r' {
+                s.pop();
+            }
+            Some((s, left))
+        }
+        let content = tokio::fs::read_to_string(&path).await.ok()?;
+        let (sig, group_cfg) = take_line(content)?;
+        let (group, cfg) = take_line(group_cfg)?;
+
+        if group != self.name {
+            log::warn!("group name changed: {path} '{}' -> '{}'", self.name, group);
+            return None;
+        }
+        log::info!("load from snapshot: {} {} => {}", path, self.sig, sig);
+        self.sig = sig;
+        self.cfg = cfg;
+        self.changed = true;
+        Some(())
+    }
+    // 第一行是sig
+    // 第二行是group name
+    // 后面是cfg
+    async fn _dump_to_snapshot(&self, snapshot: &str) -> std::io::Result<()> {
+        let path = format!("{}/{}", snapshot, self.local_path);
+        log::info!("dump to snapshot: {}", path);
+        let mut file = File::create(path).await?;
+        file.write_all(&self.sig.as_bytes()).await?;
+        file.write_all(b"\n").await?;
+        file.write_all(&self.name.as_bytes()).await?;
+        file.write_all(b"\n").await?;
+        file.write_all(&self.cfg.as_bytes()).await?;
+        Ok(())
+    }
+    async fn dump_to_snapshot(&self, snapshot: &str) {
+        if let Err(e) = self._dump_to_snapshot(snapshot).await {
+            log::warn!("failed to dump to snapshot: {}, {}", self.name, e);
+        }
+    }
+    async fn load_from_discover<D: Discover>(&mut self, snapshot: &str, d: &D) {
+        log::debug!("load from discover: {}", self.path);
+        use crate::Config;
+        match d.get_service::<String>(&self.path, &self.sig).await {
+            Ok(Config::NotFound) => log::warn!("service not found: {}", self.path),
+            Ok(Config::NotChanged) => log::debug!("service not changed: {}", self.path),
+            Ok(Config::Config(sig, cfg)) => {
+                log::info!("service changed: {} sig {} => {}", self.path, self.sig, sig);
+                self.sig = sig;
+                self.cfg = cfg;
+                self.dump_to_snapshot(snapshot).await;
+                self.cache.clear();
+                self.changed = true;
+            }
+            Err(e) => log::error!("failed to get service: {}, {}", self.path, e),
+        }
+    }
+    async fn refresh_new<D: Discover>(&mut self, snapshot: &str, d: &D) {
+        if self.cfg.len() == 0 {
+            self.load_from_discover(snapshot, d).await;
+        }
+        self.update_all(true);
+    }
+    async fn refresh<D: Discover>(&mut self, snapshot: &str, d: &D) {
+        self.load_from_discover(snapshot, d).await;
+        self.update_all(false);
+    }
+    // new: 只更新新注册的服务
+    fn update_all(&mut self, new: bool) {
+        if self.changed && self.namespaces.len() > 0 {
+            let s = self.namespaces.first().expect("empty");
+            self.cache = s
+                .top
+                .disgroup(&self.name, &self.cfg)
+                .into_iter()
+                .map(|(k, v)| (k.to_string(), v.to_string()))
+                .collect();
+            for s in self.namespaces.iter_mut() {
+                // 如果是新注册的，则不更新
+                if new && s.cfg.len() > 0 {
+                    continue;
+                }
+                let ns = &s.name;
+                match self.cache.get(ns) {
+                    Some(cfg) => s.update(cfg),
+                    None => log::warn!("namespace {} not found", ns),
+                }
+            }
+            self.changed = false;
+        }
+    }
+    fn register(&mut self, ns: String, top: T) {
+        assert!(self.namespaces.iter().find(|s| s.name == ns).is_none());
+        let service = Service::new(ns, top);
+        self.namespaces.push(service);
+        self.changed = true;
+    }
+    fn new(name: String, local_path: String, path: String) -> Self {
+        Self {
+            name,
+            path,
+            local_path,
+            sig: String::new(),
+            cfg: String::new(),
+            namespaces: Vec::new(),
+            cache: HashMap::new(),
+            changed: false,
+        }
+    }
+}
+
+struct Service<T> {
+    name: String,
+    top: T,
+    cfg: String,
+}
+impl<T: TopologyWrite> Service<T> {
+    fn new(name: String, top: T) -> Self {
+        Self {
+            name,
+            top,
+            cfg: String::new(),
+        }
+    }
+    fn update(&mut self, cfg: &str) {
+        if cfg != self.cfg {
+            self.cfg = cfg.to_string();
+            self.top.update(&self.name, cfg);
+            log::info!("Updated {}", self.name);
         }
     }
 }
