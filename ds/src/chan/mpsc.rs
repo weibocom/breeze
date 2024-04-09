c0d33a7b0b46414d69040025e13f43d00709bbcb
icycrystal4 <icycrystal4@gmail.com>
2024-04-09 18:34:48 +0800

    handler: 支持指从chan中poll_recv_many

--- ds/src/chan/mpsc.rs
+++ ds/src/chan/mpsc.rs
@@ -1,7 +1,5 @@
 use core::fmt;
 use std::fmt::{Debug, Formatter};
-use std::sync::atomic::{AtomicUsize, Ordering::*};
-use std::sync::Arc;
 use std::task::{Context, Poll};
 
 use crate::Switcher;
@@ -15,29 +13,22 @@ pub enum TrySendError<T> {
 pub fn channel<T>(cap: usize) -> (Sender<T>, Receiver<T>) {
     let s: Switcher = false.into();
     let (tx, rx) = tokio::sync::mpsc::channel(cap);
-    let num = Arc::new(AtomicUsize::new(0));
     let tx = Sender {
         switcher: s.clone(),
         inner: tx,
-        tx: num.clone(),
     };
     let rx = Receiver {
         switcher: s,
         inner: rx,
-        rx: 0,
-        tx: num,
     };
     (tx, rx)
 }
 
 pub struct Receiver<T> {
-    rx: usize,
     switcher: Switcher,
     inner: tokio::sync::mpsc::Receiver<T>,
-    tx: Arc<AtomicUsize>,
 }
 pub struct Sender<T> {
-    tx: Arc<AtomicUsize>,
     switcher: Switcher,
     inner: tokio::sync::mpsc::Sender<T>,
 }
@@ -50,29 +41,23 @@ impl<T> Sender<T> {
 
 impl<T> Receiver<T> {
     #[inline(always)]
-    pub fn poll_recv(&mut self, cx: &mut Context<'_>) -> Poll<Option<T>> {
-        self.inner.poll_recv(cx).map(|t| {
-            t.map(|v| {
-                self.rx += 1;
-                v
-            })
-        })
+    pub fn poll_recv_many(
+        &mut self,
+        cx: &mut Context<'_>,
+        buffer: &mut Vec<T>,
+        limit: usize,
+    ) -> Poll<usize> {
+        self.inner.poll_recv_many(cx, buffer, limit)
     }
     #[inline(always)]
-    pub fn has_multi(&mut self) -> bool {
-        self.tx.load(Acquire) >= self.rx + 2
+    pub fn poll_recv(&mut self, cx: &mut Context<'_>) -> Poll<Option<T>> {
+        self.inner.poll_recv(cx)
     }
     pub fn enable(&mut self) {
-        // assert_eq!(self.tx.load(Acquire), self.rx, "not empty after disable");
-        self.rx = 0;
-        self.tx.store(0, Release);
         self.switcher.on();
     }
     pub fn disable(&mut self) {
         self.switcher.off();
-    }
-    pub fn is_empty_hint(&mut self) -> bool {
-        self.rx >= self.tx.load(Acquire)
     }
 }
 impl<T> Debug for Receiver<T> {
@@ -80,9 +65,7 @@ impl<T> Debug for Receiver<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "mpsc Receiver rx/tx:({},{}) switcher:{} inner:{:?}",
-            self.rx,
-            self.tx.load(Acquire),
+            "mpsc Receiver switcher:{} inner:{:?}",
             self.switcher.get(),
             self.inner
         )
@@ -93,13 +76,9 @@ impl<T> Sender<T> {
     #[inline]
     pub fn try_send(&self, message: T) -> Result<(), TrySendError<T>> {
         if self.switcher.get() {
-            self.tx.fetch_add(1, AcqRel);
-            self.inner.try_send(message).map_err(|e| {
-                self.tx.fetch_sub(1, AcqRel);
-                match e {
-                    tokio::sync::mpsc::error::TrySendError::Full(t) => TrySendError::Full(t),
-                    tokio::sync::mpsc::error::TrySendError::Closed(t) => TrySendError::Closed(t),
-                }
+            self.inner.try_send(message).map_err(|e| match e {
+                tokio::sync::mpsc::error::TrySendError::Full(t) => TrySendError::Full(t),
+                tokio::sync::mpsc::error::TrySendError::Closed(t) => TrySendError::Closed(t),
             })?;
             Ok(())
         } else {
