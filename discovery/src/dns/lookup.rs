bc00885e1e522df4438ba280effc44692d5c2e02
parabala <codernew@163.com>
2024-04-18 12:14:18 +0800

    dns解析使用trust_dns_resolver

--- discovery/src/dns/lookup.rs
+++ discovery/src/dns/lookup.rs
@@ -1,40 +1,38 @@
-use super::{Ipv4Vec, Record};
-pub(super) struct Lookup {}
-
-use dns_lookup::{getaddrinfo, AddrInfoHints};
-use libc::SOCK_STREAM;
+use super::{Ipv4Vec, Record, Resolver};
+pub(super) struct Lookup {
+    resolver: Resolver,
+}
 
 use std::net::IpAddr;
 impl Lookup {
     pub(super) fn new() -> Self {
-        Self {}
+        Self {
+            resolver: Resolver::tokio_from_system_conf().expect("Failed to create the resolver"),
+        }
     }
-    fn dns_lookup(&self, host: &str) -> std::io::Result<Ipv4Vec> {
-        let hints = AddrInfoHints {
-            socktype: SOCK_STREAM as i32,
-            ..AddrInfoHints::default()
-        };
-
+    async fn dns_lookup(&self, host: &str) -> std::io::Result<Ipv4Vec> {
         let mut ipv4vec = super::Ipv4Vec::new();
-
-        let addrs = getaddrinfo(Some(host), None, Some(hints))?;
-        for ip in addrs {
-            let ip = ip?;
-            match ip.sockaddr.ip() {
-                IpAddr::V4(ip) => ipv4vec.push(ip),
-                IpAddr::V6(_) => {}
+        match self.resolver.lookup_ip(host.to_string()).await {
+            Ok(ips) => {
+                for ip in ips.iter() {
+                    match ip {
+                        IpAddr::V4(ip) => ipv4vec.push(ip),
+                        IpAddr::V6(_) => {}
+                    }
+                }
             }
+            Err(e) => log::info!("refresh host failed:{}, {:?}", host, e),
         }
         Ok(ipv4vec)
     }
-    pub(super) fn lookups<'a, I>(&self, iter: I) -> (usize, Option<String>)
+    pub(super) async fn lookups<'a, I>(&self, iter: I) -> std::io::Result<(usize, Option<String>)>
     where
         I: Iterator<Item = (&'a str, &'a mut Record)>,
     {
         let mut num = 0;
         let mut cache = None;
         for (host, r) in iter {
-            let ret = self.dns_lookup(host);
+            let ret = self.dns_lookup(host).await;
             if ret.is_err() {
                 log::error!("Failed to lookup ip for {} err:{:?}", host, ret.err());
                 break;
@@ -45,7 +43,7 @@ impl Lookup {
                 (num == 1).then(|| cache = Some(host.to_string()));
             }
         }
-        (num, cache)
+        Ok((num, cache))
     }
 }
 
