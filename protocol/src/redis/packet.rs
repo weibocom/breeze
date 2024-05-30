962ab4ca44be867ecfe4e1efd5aff1d87b8c9638
parabala <codernew@163.com>
2024-05-30 11:23:48 +0800

    redis响应消息里面bulk数量大于100才输出监控数据

--- protocol/src/redis/packet.rs
+++ protocol/src/redis/packet.rs
@@ -550,7 +550,7 @@ impl Packet {
     #[inline]
     pub fn skip_all_bulk(&self, oft: &mut usize) -> Result<()> {
         let bulk_count = self.num_of_bulks(oft)?;
-        if bulk_count > 10 {
+        if bulk_count > 100 {
             metrics::incr_proto_incomplete();
         }
         self.skip_bulk(oft, bulk_count)
