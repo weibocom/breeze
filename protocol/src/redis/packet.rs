c2e5dbe1b6bbebb8133258eb070a57cbfef11bdc
parabala <codernew@163.com>
2024-06-11 11:30:02 +0800

    暂存redis array解析中间结果

--- protocol/src/redis/packet.rs
+++ protocol/src/redis/packet.rs
@@ -583,6 +583,35 @@ impl Packet {
         }
         Ok(())
     }
+    #[inline]
+    pub fn skip_multibulks(&self, oft: &mut usize, bulks: &mut usize) -> Result<()> {
+        if *bulks == 0 {
+            *bulks = self.num_of_bulks(oft)?;
+        }
+        self.skip_multibulks_inner(oft, bulks)
+    }
+    #[inline]
+    pub fn skip_multibulks_inner(&self, oft: &mut usize, bulks: &mut usize) -> Result<()> {
+        while *bulks > 0 {
+            self.check_onetoken(*oft)?;
+            match self.at(*oft) {
+                b'*' => {
+                    *bulks += self.num_of_bulks(oft)? - 1;
+                }
+                b'$' => {
+                    // 跳过num个字节 + "\r\n" 2个字节
+                    *oft += self.num_of_string(oft)? + CRLF_LEN;
+                    (*bulks > 0).then(|| *bulks -= 1);
+                }
+                b'+' | b':' => {
+                    self.line(oft)?;
+                    (*bulks > 0).then(|| *bulks -= 1);
+                }
+                _ => panic!("unsupport rsp:{:?}, pos: {}/{:?}", self, oft, bulks),
+            }
+        }
+        Ok(())
+    }
 }
 
 use std::sync::atomic::{AtomicI64, Ordering};
