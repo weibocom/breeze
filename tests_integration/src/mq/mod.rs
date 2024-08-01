cedf32e43b5a13fdee9d8ab63b3d193b3285290e
hustfisher <hustfisher@gmail.com>
2024-08-01 15:57:55 +0800

    update for test

--- tests_integration/src/mq/mod.rs
+++ tests_integration/src/mq/mod.rs
@@ -149,7 +149,7 @@ fn msgque_strategy_check() {
     }
 
     let hits_percent = (hits as f64) / (read_count as f64);
-    println!("hits percent: {}", hits_percent);
+    println!("hits percent1: {}", hits_percent);
     // assert!(
     //     hits_percent >= 0.8,
     //     "strategy check read strategy:{}/{}",
